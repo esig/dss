fff42ea27f01cd8831859bcf26e30c2609a4028e
david.naramski <david.naramski@nowina.lu>
2015-03-10 15:17:37 +0100

    Test should not break when no internet connection

--- dss-document/src/test/java/eu/europa/ec/markt/dss/validation102853/tsl/TrustedListCertificateSourceTest.java
+++ dss-document/src/test/java/eu/europa/ec/markt/dss/validation102853/tsl/TrustedListCertificateSourceTest.java
@@ -1,25 +1,34 @@
 package eu.europa.ec.markt.dss.validation102853.tsl;
 
+import java.net.UnknownHostException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import eu.europa.ec.markt.dss.validation102853.https.CommonsDataLoader;
 
 public class TrustedListCertificateSourceTest {
-	
+
 	@Test
 	public void test1() throws Exception {
-		
-		TrustedListsCertificateSource source = new TrustedListsCertificateSource();
-		source.setDataLoader(new CommonsDataLoader());
-		source.setLotlCertificate("classpath://ec.europa.eu.crt");
-		source.setLotlUrl("https://ec.europa.eu/information_society/policy/esignature/trusted-list/tl-mp.xml");
-		source.setTslRefreshPolicy(TSLRefreshPolicy.NEVER);
-		source.setCheckSignature(false);
-		source.init();
-		
-		Assert.assertTrue(source.getCertificates().size() > 0);
+
+		try {
+			TrustedListsCertificateSource source = new TrustedListsCertificateSource();
+			source.setDataLoader(new CommonsDataLoader());
+			source.setLotlCertificate("classpath://ec.europa.eu.crt");
+			source.setLotlUrl("https://ec.europa.eu/information_society/policy/esignature/trusted-list/tl-mp.xml");
+			source.setTslRefreshPolicy(TSLRefreshPolicy.NEVER);
+			source.setCheckSignature(false);
+			source.init();
+
+			Assert.assertTrue(source.getCertificates().size() > 0);
+		} catch (Exception e) {
+			if(!(e.getCause() instanceof UnknownHostException)) {
+				throw e;
+			}
+			// Internet failure is not test failure
+		}
 
 	}
-	
+
 }
