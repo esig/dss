e790d06885afa1cf53b4a8fd2f5f5ca415b9fa7b
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2015-06-10 14:05:35 +0200

    Use DSSUtils to load Certificate

--- dss-spi/src/test/java/eu/europa/esig/dss/RSASSAPSSTest.java
+++ dss-spi/src/test/java/eu/europa/esig/dss/RSASSAPSSTest.java
@@ -4,14 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import eu.europa.esig.dss.x509.CertificateToken;
-import eu.europa.esig.dss.x509.BCTokenBuilder;
 
 public class RSASSAPSSTest {
 
 	@Test
 	public void test() {
 
-		CertificateToken token = new BCTokenBuilder().buildCertificateToken(this.getClass().getResourceAsStream("/BA-QC-Wurzel-CA-2_PN.txt"));
+		CertificateToken token = DSSUtils.loadCertificate(this.getClass().getResourceAsStream("/BA-QC-Wurzel-CA-2_PN.txt"));
 
 		Assert.assertTrue(token.isSelfSigned());
 		Assert.assertTrue(token.isSignedBy(token));
