50bb164ef1929bfed36673b751419008d04baec6
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2015-06-19 09:04:31 +0200

    DSS-680 : CommonsDataLoader and Proxy improvements (enable edition from webapp)

--- dss-demo-webapp/src/main/resources/load.sql
+++ dss-demo-webapp/src/main/resources/load.sql
@@ -1,12 +1,16 @@
 INSERT INTO PREFERENCES (PREF_KEY , PREF_VALUE) values ( 'preference.url.service', 'http://localhost:8080/wservice');
 INSERT INTO PREFERENCES (PREF_KEY , PREF_VALUE) values ( 'preference.default.policy.url', null);
+
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.http.host', '127.0.0.1');
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.http.port', '8008');
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.http.user', '');
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.http.password', '');
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.http.enabled', 'false');
+INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.http.exclude', '');
+
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.https.host', '127.0.0.1');
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.https.port', '8008');
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.https.user', '');
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.https.password', '');
 INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.https.enabled', 'false');
+INSERT INTO PROXY_PREFERENCES (PROXY_KEY , PROXY_VALUE) values ( 'proxy.https.exclude', '');
