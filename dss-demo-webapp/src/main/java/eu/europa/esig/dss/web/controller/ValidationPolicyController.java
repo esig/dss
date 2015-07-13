0dd41c447134a1fae79c399ab0bce4f9eece7365
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2015-07-13 14:43:43 +0200

    Simplify policy.xsd, oxm integration, start jsp

---
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/controller/ValidationPolicyController.java
@@ -0,0 +1,49 @@
+package eu.europa.esig.dss.web.controller;
+
+import java.io.InputStream;
+
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import eu.europa.esig.dss.validation.ValidationResourceManager;
+import eu.europa.esig.jaxb.policy.ConstraintsParameters;
+
+@Controller
+@RequestMapping(value = "/validation-policy")
+public class ValidationPolicyController {
+
+	private static final Logger logger = LoggerFactory.getLogger(ValidationPolicyController.class);
+
+	private static final String VALIDATION_POLICY_TILE = "validation-policy";
+
+	@Autowired
+	private Unmarshaller policyMarshaller;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String showValidationPolicy(Model model) {
+
+		InputStream is = null;
+		ConstraintsParameters policy = null;
+		try {
+			is = ValidationPolicyController.class.getResourceAsStream(ValidationResourceManager.defaultPolicyConstraintsLocation);
+			policy = (ConstraintsParameters) policyMarshaller.unmarshal(new StreamSource(is));
+		} catch (Exception e) {
+			logger.error("Unable to parse '" + ValidationResourceManager.defaultPolicyConstraintsLocation + "' : " + e.getMessage(), e);
+		} finally {
+			IOUtils.closeQuietly(is);
+		}
+
+		model.addAttribute("policy", policy);
+
+		return VALIDATION_POLICY_TILE;
+	}
+}
--- dss-demo-webapp/src/main/webapp/WEB-INF/templates/menuLayout.jsp
+++ dss-demo-webapp/src/main/webapp/WEB-INF/templates/menuLayout.jsp
@@ -17,6 +17,7 @@
             <a href="<spring:url value="/signature" />" class="list-group-item${currentPage eq '/signature' ? ' active' : ''}"><spring:message code="label.signature.applet" /></a>
             <a href="<spring:url value="/extension" />" class="list-group-item${currentPage eq '/extension' ? ' active' : ''}"><spring:message code="label.extend" /></a>
             <a href="<spring:url value="/validation" />" class="list-group-item${currentPage eq '/validation' ? ' active' : ''}"><spring:message code="label.validate" /></a>
+            <a href="<spring:url value="/validation-policy" />" class="list-group-item${currentPage eq '/validation-policy' ? ' active' : ''}"><spring:message code="label.validation-policy" /></a>
         </c:otherwise>
     </c:choose>
 </div>
