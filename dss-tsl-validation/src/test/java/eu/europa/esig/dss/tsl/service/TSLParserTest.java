abee49fc4eb3dbd66f7dccd2cc38f09fc2a56747
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2015-08-19 17:33:09 +0200

    Add tests to be sure to retrieve all certificates

--- dss-tsl-validation/src/test/java/eu/europa/esig/dss/tsl/service/TSLParserTest.java
+++ dss-tsl-validation/src/test/java/eu/europa/esig/dss/tsl/service/TSLParserTest.java
@@ -6,7 +6,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
@@ -14,6 +16,9 @@ import org.junit.Test;
 
 import eu.europa.esig.dss.tsl.TSLParserResult;
 import eu.europa.esig.dss.tsl.TSLPointer;
+import eu.europa.esig.dss.tsl.TSLService;
+import eu.europa.esig.dss.tsl.TSLServiceProvider;
+import eu.europa.esig.dss.x509.CertificateToken;
 
 public class TSLParserTest {
 
@@ -35,6 +40,40 @@ public class TSLParserTest {
 			assertTrue(CollectionUtils.isNotEmpty(tslPointer.getPotentialSigners()));
 		}
 		assertTrue(CollectionUtils.isNotEmpty(model.getDistributionPoints()));
+	}
+
+	@Test
+	public void countCertificatesLT() throws Exception {
+		int oldResult = 35;
+		TSLParser parser = new TSLParser(new FileInputStream(new File("src/test/resources/tsls/621C7723265CA33AAD0607B3C612B313872E7514.xml")));
+		TSLParserResult model = parser.call();
+
+		Set<CertificateToken> certs = new HashSet<CertificateToken>();
+		List<TSLServiceProvider> serviceProviders = model.getServiceProviders();
+		for (TSLServiceProvider tslServiceProvider : serviceProviders) {
+			List<TSLService> services = tslServiceProvider.getServices();
+			for (TSLService tslService : services) {
+				certs.addAll( tslService.getCertificates());
+			}
+		}
+		assertEquals(oldResult, certs.size());
+	}
+
+	@Test
+	public void countCertificatesDE() throws Exception {
+		int oldResult = 413;
+		TSLParser parser = new TSLParser(new FileInputStream(new File("src/test/resources/tsls/59F95095730A1809A027655246D6524959B191A8.xml")));
+		TSLParserResult model = parser.call();
+
+		Set<CertificateToken> certs = new HashSet<CertificateToken>();
+		List<TSLServiceProvider> serviceProviders = model.getServiceProviders();
+		for (TSLServiceProvider tslServiceProvider : serviceProviders) {
+			List<TSLService> services = tslServiceProvider.getServices();
+			for (TSLService tslService : services) {
+				certs.addAll( tslService.getCertificates());
+			}
+		}
+		assertEquals(oldResult, certs.size());
 	}
 
 }
