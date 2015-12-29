aa886fc74a1afd7b7abaffd271b3b2cb8d95ea11
Axel Abinet <axel.abinet@nowina.lu>
2015-12-29 09:09:04 +0100

    add check fort the signature policy identifier

--- validation-policy/src/main/java/eu/europa/esig/dss/EN319102/bbb/vci/ValidationContextInitialization.java
+++ validation-policy/src/main/java/eu/europa/esig/dss/EN319102/bbb/vci/ValidationContextInitialization.java
@@ -1,16 +1,48 @@
 package eu.europa.esig.dss.EN319102.bbb.vci;
 
+import eu.europa.esig.dss.EN319102.bbb.AbstractBasicBuildingBlock;
+import eu.europa.esig.dss.EN319102.bbb.ChainItem;
+import eu.europa.esig.dss.EN319102.bbb.vci.checks.SignaturePolicyIdentifierCheck;
+import eu.europa.esig.dss.jaxb.detailedreport.XmlVCI;
+import eu.europa.esig.dss.jaxb.detailedreport.XmlXCV;
+import eu.europa.esig.dss.validation.SignatureWrapper;
+import eu.europa.esig.dss.validation.policy.ValidationPolicy2;
+import eu.europa.esig.jaxb.policy.LevelConstraint;
+
 /**
  * 5.2.4 Validation context initialization
  * This building block initializes the validation constraints (chain constraints, cryptographic constraints, signature
  * elements constraints) and parameters (X.509 validation parameters including trust anchors, certificate validation data)
  * that will be used to validate the signature.
  */
-public class ValidationContextInitialization {
+public class ValidationContextInitialization extends AbstractBasicBuildingBlock<XmlVCI> {
 
-	public void execute() {
-		// TODO Auto-generated method stub
-
+	private final SignatureWrapper signature;
+	private final ValidationPolicy2 validationPolicy;
+	
+	private ChainItem<XmlVCI> firstItem;
+	private XmlVCI result = new XmlVCI();
+	
+	
+	public ValidationContextInitialization(SignatureWrapper signature, ValidationPolicy2 validationPolicy) {
+		this.signature = signature;
+		this.validationPolicy = validationPolicy;
+	}
+	
+	@Override
+	public void initChain() {
+		firstItem = signaturePolicyIdentifier();
+	}
+	
+	private ChainItem<XmlVCI> signaturePolicyIdentifier() {
+		LevelConstraint constraint = validationPolicy.getStructuralValidationConstraint();
+		return new SignaturePolicyIdentifierCheck(result, constraint, signature);
+	}
+	
+	@Override
+	public XmlVCI execute() {
+		firstItem.execute();
+		return result;
 	}
 
 }
---
+++ validation-policy/src/main/java/eu/europa/esig/dss/EN319102/bbb/vci/checks/SignaturePolicyIdentifierCheck.java
@@ -0,0 +1,54 @@
+package eu.europa.esig.dss.EN319102.bbb.vci.checks;
+
+import eu.europa.esig.dss.EN319102.bbb.ChainItem;
+import eu.europa.esig.dss.jaxb.detailedreport.XmlVCI;
+import eu.europa.esig.dss.validation.SignatureWrapper;
+import eu.europa.esig.dss.validation.policy.rules.Indication;
+import eu.europa.esig.dss.validation.policy.rules.MessageTag;
+import eu.europa.esig.dss.validation.policy.rules.SubIndication;
+import eu.europa.esig.dss.x509.SignaturePolicy;
+import eu.europa.esig.jaxb.policy.LevelConstraint;
+
+public class SignaturePolicyIdentifierCheck extends ChainItem<XmlVCI> {
+	
+	private final SignatureWrapper signature;
+	private final LevelConstraint constraint;
+
+	public SignaturePolicyIdentifierCheck(XmlVCI result, LevelConstraint constraint, SignatureWrapper signature) {
+		super(result, constraint);
+		this.signature = signature;
+		this.constraint = constraint;
+	}
+
+	@Override
+	protected boolean process() {
+		if(signature.getPolicyId().equals(SignaturePolicy.IMPLICIT_POLICY)) {
+			return true;
+		} else if(signature.isPolicyPresent()) {
+			return signature.getPolicyStatus();
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	protected MessageTag getMessageTag() {
+		return MessageTag.BBB_VCI_ISPK;
+	}
+
+	@Override
+	protected MessageTag getErrorMessageTag() {
+		return signature.isPolicyPresent() ? MessageTag.BBB_VCI_ISPK_ANS_2 : MessageTag.BBB_VCI_ISPK_ANS_1;
+	}
+
+	@Override
+	protected Indication getFailedIndicationForConclusion() {
+		return Indication.INDETERMINATE;
+	}
+
+	@Override
+	protected SubIndication getFailedSubIndicationForConclusion() {
+		return signature.isPolicyPresent() ? SubIndication.POLICY_PROCESSING_ERROR : SubIndication.NO_POLICY;
+	}
+
+}
