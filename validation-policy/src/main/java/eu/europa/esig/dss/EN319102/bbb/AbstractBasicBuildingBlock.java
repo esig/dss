e3e343e99c859e43c8b02916673ef8fba92140ae
Axel Abinet <axel.abinet@nowina.lu>
2016-01-05 17:02:06 +0100

    creation of a conclusion in every case

--- validation-policy/src/main/java/eu/europa/esig/dss/EN319102/bbb/AbstractBasicBuildingBlock.java
+++ validation-policy/src/main/java/eu/europa/esig/dss/EN319102/bbb/AbstractBasicBuildingBlock.java
@@ -1,6 +1,8 @@
 package eu.europa.esig.dss.EN319102.bbb;
 
+import eu.europa.esig.dss.jaxb.detailedreport.XmlConclusion;
 import eu.europa.esig.dss.jaxb.detailedreport.XmlConstraintsConclusion;
+import eu.europa.esig.dss.validation.policy.rules.Indication;
 
 public abstract class AbstractBasicBuildingBlock<T extends XmlConstraintsConclusion> {
 
@@ -15,6 +17,11 @@ public abstract class AbstractBasicBuildingBlock<T extends XmlConstraintsConclus
 	public T execute() {
 		initChain();
 		firstItem.execute();
+		if(result.getConclusion() == null) {
+			XmlConclusion conclusion = new XmlConclusion();
+			conclusion.setIndication(Indication.VALID);
+			result.setConclusion(conclusion);
+		}
 		return result;
 	}
 
