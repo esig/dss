0579d53e2f255d348878f688037c9b47c0a8cbe7
Axel Abinet <axel.abinet@nowina.lu>
2016-01-06 09:30:25 +0100

    Tests for the cryptographic verification

---
+++ validation-policy/src/test/java/eu/europa/esig/dss/validation/policy/bbb/CryptographicVerificationTest.java
@@ -0,0 +1,69 @@
+package eu.europa.esig.dss.validation.policy.bbb;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import eu.europa.esig.dss.EN319102.bbb.cv.CryptographicVerification;
+import eu.europa.esig.dss.EN319102.policy.ValidationPolicy.Context;
+import eu.europa.esig.dss.jaxb.detailedreport.XmlCV;
+import eu.europa.esig.dss.validation.policy.bbb.util.TestDiagnosticDataGenerator;
+import eu.europa.esig.dss.validation.policy.bbb.util.TestPolicyGenerator;
+import eu.europa.esig.dss.validation.policy.rules.Indication;
+import eu.europa.esig.dss.validation.policy.rules.SubIndication;
+import eu.europa.esig.dss.validation.report.DiagnosticData;
+import eu.europa.esig.jaxb.policy.Level;
+import eu.europa.esig.jaxb.policy.LevelConstraint;
+
+public class CryptographicVerificationTest {
+
+	@Test
+	public void CryptographicVerificationWithBasicDataTest() throws Exception {
+		DiagnosticData diagnosticData = TestDiagnosticDataGenerator.generateSimpleDiagnosticData();
+		
+		LevelConstraint failLevel = new LevelConstraint();
+		failLevel.setLevel(Level.FAIL);
+		
+		CryptographicVerification verification = new CryptographicVerification(diagnosticData.getSignatures().get(0), Context.SIGNATURE, TestPolicyGenerator.generatePolicy());
+		XmlCV xcv = verification.execute();
+		Assert.assertEquals(Indication.VALID, xcv.getConclusion().getIndication());
+	}
+	
+	@Test
+	public void CryptographicVerificationWithSignatureNonIntactTest() throws Exception {
+		DiagnosticData diagnosticData = TestDiagnosticDataGenerator.generateDiagnosticDataWithNonIntactSignature();
+		
+		LevelConstraint failLevel = new LevelConstraint();
+		failLevel.setLevel(Level.FAIL);
+		
+		CryptographicVerification verification = new CryptographicVerification(diagnosticData.getSignatures().get(0), Context.SIGNATURE, TestPolicyGenerator.generatePolicy());
+		XmlCV xcv = verification.execute();
+		Assert.assertEquals(Indication.INVALID, xcv.getConclusion().getIndication());
+		Assert.assertEquals(SubIndication.SIG_CRYPTO_FAILURE, xcv.getConclusion().getSubIndication());
+	}
+	
+	@Test
+	public void CryptographicVerificationWithDataReferenceNonIntactTest() throws Exception {
+		DiagnosticData diagnosticData = TestDiagnosticDataGenerator.generateDiagnosticReferenceDataWithNonIntactSignature();
+		
+		LevelConstraint failLevel = new LevelConstraint();
+		failLevel.setLevel(Level.FAIL);
+		
+		CryptographicVerification verification = new CryptographicVerification(diagnosticData.getSignatures().get(0), Context.SIGNATURE, TestPolicyGenerator.generatePolicy());
+		XmlCV xcv = verification.execute();
+		Assert.assertEquals(Indication.INVALID, xcv.getConclusion().getIndication());
+		Assert.assertEquals(SubIndication.HASH_FAILURE, xcv.getConclusion().getSubIndication());
+	}
+	
+	@Test
+	public void CryptographicVerificationWithDataReferenceNotFoundTest() throws Exception {
+		DiagnosticData diagnosticData = TestDiagnosticDataGenerator.generateDiagnosticReferenceDataWithNotFound();
+		
+		LevelConstraint failLevel = new LevelConstraint();
+		failLevel.setLevel(Level.FAIL);
+		
+		CryptographicVerification verification = new CryptographicVerification(diagnosticData.getSignatures().get(0), Context.SIGNATURE, TestPolicyGenerator.generatePolicy());
+		XmlCV xcv = verification.execute();
+		Assert.assertEquals(Indication.INDETERMINATE, xcv.getConclusion().getIndication());
+		Assert.assertEquals(SubIndication.SIGNED_DATA_NOT_FOUND, xcv.getConclusion().getSubIndication());
+	}
+}
