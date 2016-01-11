fb17e14fa1a21c26b4538f49699fe5b2dc3e82f6
Axel Abinet <axel.abinet@nowina.lu>
2016-01-11 12:54:25 +0100

    Remove save

--- dss-xades/src/test/java/eu/europa/esig/dss/xades/extension/XAdESBWithoutSignedDataObjectPropertiesToLTATest.java
+++ dss-xades/src/test/java/eu/europa/esig/dss/xades/extension/XAdESBWithoutSignedDataObjectPropertiesToLTATest.java
@@ -1,26 +1,17 @@
 package eu.europa.esig.dss.xades.extension;
 
-import java.io.IOException;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 import eu.europa.esig.dss.DSSDocument;
 import eu.europa.esig.dss.DigestAlgorithm;
-import eu.europa.esig.dss.EncryptionAlgorithm;
 import eu.europa.esig.dss.FileDocument;
 import eu.europa.esig.dss.SignatureAlgorithm;
 import eu.europa.esig.dss.SignatureLevel;
 import eu.europa.esig.dss.SignaturePackaging;
-import eu.europa.esig.dss.SignatureValue;
-import eu.europa.esig.dss.ToBeSigned;
-import eu.europa.esig.dss.test.TestUtils;
 import eu.europa.esig.dss.test.gen.CertificateService;
 import eu.europa.esig.dss.test.mock.MockPrivateKeyEntry;
-import eu.europa.esig.dss.test.mock.MockSignatureTokenConnection;
 import eu.europa.esig.dss.test.mock.MockTSPSource;
-import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
-import eu.europa.esig.dss.validation.CertificateVerifier;
 import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 import eu.europa.esig.dss.validation.SignedDocumentValidator;
 import eu.europa.esig.dss.validation.report.DiagnosticData;
@@ -48,7 +39,6 @@ public class XAdESBWithoutSignedDataObjectPropertiesToLTATest {
 		parameters.setDigestAlgorithm(DigestAlgorithm.SHA256);
 		
 		DSSDocument extendDocument = service.extendDocument(toSignDocument, parameters);
-		extendDocument.save("src/test/resources/result.xml");
 		
 		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(extendDocument);
 		validator.setCertificateVerifier(new CommonCertificateVerifier());
