dbc45ad6cd576ddf4d510f3cfef31708bd6de018
Axel Abinet <axel.abinet@nowina.lu>
2016-01-25 10:21:52 +0100

    add unit test for soap and rest validation service

--- dss-validation-rest-client/src/test/java/integration/RestDocumentValidationTest.java
+++ dss-validation-rest-client/src/test/java/integration/RestDocumentValidationTest.java
@@ -1,6 +1,11 @@
 package integration;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,6 +21,7 @@ import eu.europa.esig.dss.validation.policy.rules.Indication;
 import eu.europa.esig.dss.validation.reports.Reports;
 import eu.europa.esig.dss.validation.reports.dto.DataToValidateDTO;
 import eu.europa.esig.dss.validation.reports.dto.ReportsDTO;
+import eu.europa.esig.jaxb.policy.ConstraintsParameters;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/test-context.xml")
@@ -39,6 +45,51 @@ public class RestDocumentValidationTest {
 		Assert.assertEquals(1, result.getSimpleReport().getSignature().size());
 		Assert.assertEquals(2, result.getDiagnosticData().getSignature().get(0).getTimestamps().getTimestamp().size());
 		Assert.assertTrue(result.getSimpleReport().getSignature().get(0).getIndication().equals(Indication.VALID));
+		
+		Reports reports = new Reports(result.getDiagnosticData(), result.getDetailedReport(), result.getSimpleReport());
+		reports.print();
+	}
+	
+	@Test
+	public void testWithNoPolicyAndOriginalFile() {
+		RemoteDocument signedFile = new RemoteDocument(new FileDocument("src/test/resources/xades-detached.xml"));
+		RemoteDocument originalFile =  new RemoteDocument(new FileDocument("src/test/resources/sample.xml"));
+		
+		DataToValidateDTO toValidate = new DataToValidateDTO(signedFile, originalFile, null);
+		
+		ReportsDTO result = validationService.validateSignature(toValidate);
+		
+		Assert.assertNotNull(result.getDiagnosticData());
+		Assert.assertNotNull(result.getDetailedReport());
+		Assert.assertNotNull(result.getSimpleReport());
+		
+		Assert.assertEquals(1, result.getSimpleReport().getSignature().size());
+		Assert.assertTrue(result.getSimpleReport().getSignature().get(0).getIndication().equals(Indication.INDETERMINATE));
+		
+		Reports reports = new Reports(result.getDiagnosticData(), result.getDetailedReport(), result.getSimpleReport());
+		reports.print();
+	}
+	
+	@Test
+	public void testWithPolicyAndOriginalFile() throws Exception {
+		RemoteDocument signedFile = new RemoteDocument(new FileDocument("src/test/resources/xades-detached.xml"));
+		RemoteDocument originalFile =  new RemoteDocument(new FileDocument("src/test/resources/sample.xml"));
+		
+		JAXBContext context = JAXBContext.newInstance(ConstraintsParameters.class.getPackage().getName());
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		InputStream stream = new FileInputStream("src/test/resources/constraint.xml");
+		ConstraintsParameters policy = (ConstraintsParameters) unmarshaller.unmarshal(stream);
+		
+		DataToValidateDTO toValidate = new DataToValidateDTO(signedFile, originalFile, policy);
+		
+		ReportsDTO result = validationService.validateSignature(toValidate);
+		
+		Assert.assertNotNull(result.getDiagnosticData());
+		Assert.assertNotNull(result.getDetailedReport());
+		Assert.assertNotNull(result.getSimpleReport());
+		
+		Assert.assertEquals(1, result.getSimpleReport().getSignature().size());
+		Assert.assertTrue(result.getSimpleReport().getSignature().get(0).getIndication().equals(Indication.INVALID));
 		
 		Reports reports = new Reports(result.getDiagnosticData(), result.getDetailedReport(), result.getSimpleReport());
 		reports.print();
