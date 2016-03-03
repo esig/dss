80fc898c7d713a67c8bbf9bdd9ee24993e5e88cf
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2016-03-03 10:02:04 +0100

    Add unit test

---
+++ validation-policy/src/test/java/eu/europa/esig/dss/validation/executor/CustomProcessExecutorTest.java
@@ -0,0 +1,58 @@
+package eu.europa.esig.dss.validation.executor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import org.junit.Test;
+
+import eu.europa.esig.dss.jaxb.diagnostic.DiagnosticData;
+import eu.europa.esig.dss.validation.policy.EtsiValidationPolicy;
+import eu.europa.esig.dss.validation.policy.rules.Indication;
+import eu.europa.esig.dss.validation.reports.Reports;
+import eu.europa.esig.dss.validation.reports.SimpleReport;
+import eu.europa.esig.jaxb.policy.ConstraintsParameters;
+
+public class CustomProcessExecutorTest {
+
+	@Test
+	public void test() throws Exception {
+		FileInputStream fis = new FileInputStream("src/test/resources/diagnosticData.xml");
+		DiagnosticData diagnosticData = getJAXBObjectFromString(fis, DiagnosticData.class);
+		assertNotNull(diagnosticData);
+
+		FileInputStream policyFis = new FileInputStream("src/main/resources/policy/constraint.xml");
+		ConstraintsParameters policyJaxB = getJAXBObjectFromString(policyFis, ConstraintsParameters.class);
+		assertNotNull(policyJaxB);
+
+		CustomProcessExecutor executor = new CustomProcessExecutor();
+		executor.setDiagnosticData(diagnosticData);
+		executor.setValidationPolicy(new EtsiValidationPolicy(policyJaxB));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date currentTime = sdf.parse("03/03/2016 09:25:00");
+		executor.setCurrentTime(currentTime);
+
+		Reports reports = executor.execute();
+		assertNotNull(reports);
+
+		// reports.print();
+
+		SimpleReport simpleReport = reports.getSimpleReport();
+		assertEquals(Indication.VALID, simpleReport.getIndication(simpleReport.getFirstSignatureId()));
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T extends Object> T getJAXBObjectFromString(InputStream is, Class<T> clazz) throws Exception {
+		JAXBContext context = JAXBContext.newInstance(clazz.getPackage().getName());
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		return (T) unmarshaller.unmarshal(is);
+	}
+
+}
