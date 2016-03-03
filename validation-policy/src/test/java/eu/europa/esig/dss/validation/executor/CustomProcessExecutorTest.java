c9b0dde1b48fcbde6ca7974defa41ad8a51406fa
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2016-03-03 10:48:07 +0100

    Add unit test with past signature validation

--- validation-policy/src/test/java/eu/europa/esig/dss/validation/executor/CustomProcessExecutorTest.java
+++ validation-policy/src/test/java/eu/europa/esig/dss/validation/executor/CustomProcessExecutorTest.java
@@ -16,6 +16,8 @@ import org.junit.Test;
 import eu.europa.esig.dss.jaxb.diagnostic.DiagnosticData;
 import eu.europa.esig.dss.validation.policy.EtsiValidationPolicy;
 import eu.europa.esig.dss.validation.policy.rules.Indication;
+import eu.europa.esig.dss.validation.policy.rules.SubIndication;
+import eu.europa.esig.dss.validation.reports.DetailedReport;
 import eu.europa.esig.dss.validation.reports.Reports;
 import eu.europa.esig.dss.validation.reports.SimpleReport;
 import eu.europa.esig.jaxb.policy.ConstraintsParameters;
@@ -46,6 +48,37 @@ public class CustomProcessExecutorTest {
 
 		SimpleReport simpleReport = reports.getSimpleReport();
 		assertEquals(Indication.VALID, simpleReport.getIndication(simpleReport.getFirstSignatureId()));
+	}
+
+	@Test
+	public void testPSV() throws Exception {
+		FileInputStream fis = new FileInputStream("src/test/resources/diagnosticDataPSV.xml");
+		DiagnosticData diagnosticData = getJAXBObjectFromString(fis, DiagnosticData.class);
+		assertNotNull(diagnosticData);
+
+		FileInputStream policyFis = new FileInputStream("src/main/resources/policy/constraint.xml");
+		ConstraintsParameters policyJaxB = getJAXBObjectFromString(policyFis, ConstraintsParameters.class);
+		assertNotNull(policyJaxB);
+
+		CustomProcessExecutor executor = new CustomProcessExecutor();
+		executor.setDiagnosticData(diagnosticData);
+		executor.setValidationPolicy(new EtsiValidationPolicy(policyJaxB));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date currentTime = sdf.parse("03/03/2016 09:25:00");
+		executor.setCurrentTime(currentTime);
+
+		Reports reports = executor.execute();
+		assertNotNull(reports);
+
+		SimpleReport simpleReport = reports.getSimpleReport();
+		assertEquals(Indication.VALID, simpleReport.getIndication(simpleReport.getFirstSignatureId()));
+
+		DetailedReport detailedReport = reports.getDetailedReport();
+		assertEquals(Indication.INDETERMINATE, detailedReport.getBasicValidationIndication(simpleReport.getFirstSignatureId()));
+		assertEquals(SubIndication.OUT_OF_BOUNDS_NO_POE, detailedReport.getBasicValidationSubIndication(simpleReport.getFirstSignatureId()));
+
+		assertEquals(Indication.VALID, detailedReport.getArchiveDataValidationIndication(simpleReport.getFirstSignatureId()));
+
 	}
 
 	@SuppressWarnings("unchecked")
