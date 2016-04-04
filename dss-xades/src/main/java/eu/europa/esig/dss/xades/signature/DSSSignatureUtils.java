d2e8dd2e5102d3e0b0811fd4cfa6a3401718f45b
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2016-04-04 15:53:39 +0200

    DSS-801 : improve ECDSA

--- dss-xades/src/main/java/eu/europa/esig/dss/xades/signature/DSSSignatureUtils.java
+++ dss-xades/src/main/java/eu/europa/esig/dss/xades/signature/DSSSignatureUtils.java
@@ -50,15 +50,17 @@ public final class DSSSignatureUtils {
 	 * @return
 	 */
 	public static byte[] convertToXmlDSig(final EncryptionAlgorithm algorithm, byte[] signatureValue) {
-		if (EncryptionAlgorithm.ECDSA == algorithm || EncryptionAlgorithm.DSA == algorithm) {
-			return convertDSAandECDSAASN1toXMLDSIG(signatureValue);
+		if (EncryptionAlgorithm.ECDSA == algorithm) {
+			return convertECDSAASN1toXMLDSIG(signatureValue);
+		} else if (EncryptionAlgorithm.DSA == algorithm) {
+			return convertDSAASN1toXMLDSIG(signatureValue);
 		} else {
 			return signatureValue;
 		}
 	}
 
 	/**
-	 * Converts an ASN.1 DSA / ECDSA value to a XML Signature DSA / ECDSA Value.
+	 * Converts an ASN.1 ECDSA value to a XML Signature ECDSA Value.
 	 *
 	 * The JAVA JCE ECDSA Signature algorithm creates ASN.1 encoded (r,s) value pairs; the XML Signature requires the
 	 * core BigInteger values.
@@ -70,7 +72,56 @@ public final class DSSSignatureUtils {
 	 * @see <A HREF="http://www.w3.org/TR/xmldsig-core/#dsa-sha1">6.4.1 DSA</A>
 	 * @see <A HREF="ftp://ftp.rfc-editor.org/in-notes/rfc4050.txt">3.3. ECDSA Signatures</A>
 	 */
-	private static byte[] convertDSAandECDSAASN1toXMLDSIG(byte[] binaries) {
+	private static byte[] convertECDSAASN1toXMLDSIG(byte[] binaries) {
+		ASN1InputStream is = null;
+		try {
+			is = new ASN1InputStream(binaries);
+
+			ASN1Sequence seq = (ASN1Sequence) is.readObject();
+			if (seq.size() != 2) {
+				throw new IllegalArgumentException("ASN1 Sequence size should be 2 !");
+			}
+			ASN1Integer r = (ASN1Integer) seq.getObjectAt(0);
+			ASN1Integer s = (ASN1Integer) seq.getObjectAt(1);
+
+			byte[] rBytes = r.getValue().toByteArray();
+			int rSize = rBytes.length;
+			byte[] sBytes = s.getValue().toByteArray();
+			int sSize = sBytes.length;
+
+			int max = Math.max(rSize, sSize);
+
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream(max * 2);
+			if (sSize > rSize) {
+				buffer.write(0x00);
+			}
+			buffer.write(rBytes);
+			if (rSize > sSize) {
+				buffer.write(0x00);
+			}
+			buffer.write(sBytes);
+			return buffer.toByteArray();
+		} catch (Exception e) {
+			throw new DSSException("Unable to convert to xmlDsig : " + e.getMessage(), e);
+		} finally {
+			IOUtils.closeQuietly(is);
+		}
+	}
+
+	/**
+	 * Converts an ASN.1 DSA value to a XML Signature DSA Value.
+	 *
+	 * The JAVA JCE DSA Signature algorithm creates ASN.1 encoded (r,s) value pairs; the XML Signature requires the
+	 * core BigInteger values.
+	 *
+	 * @param binaries
+	 *            the ASN1 signature value
+	 * @return the decode bytes
+	 * @throws IOException
+	 * @see <A HREF="http://www.w3.org/TR/xmldsig-core/#dsa-sha1">6.4.1 DSA</A>
+	 * @see <A HREF="ftp://ftp.rfc-editor.org/in-notes/rfc4050.txt">3.3. ECDSA Signatures</A>
+	 */
+	private static byte[] convertDSAASN1toXMLDSIG(byte[] binaries) {
 		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 		ASN1InputStream is = null;
 		try {
@@ -80,6 +131,7 @@ public final class DSSSignatureUtils {
 			if (seq.size() != 2) {
 				throw new IllegalArgumentException("ASN1 Sequence size should be 2 !");
 			}
+
 			ASN1Integer r = (ASN1Integer) seq.getObjectAt(0);
 			ASN1Integer s = (ASN1Integer) seq.getObjectAt(1);
 
