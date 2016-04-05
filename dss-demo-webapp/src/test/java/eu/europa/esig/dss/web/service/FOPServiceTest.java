1a3a6b99be0b3b1519650a6e826749311cba4385
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2016-04-05 15:26:11 +0200

    Update unit tests for VALID -> PASSED, INVALID -> FAILED,...

--- dss-demo-webapp/src/test/java/eu/europa/esig/dss/web/service/FOPServiceTest.java
+++ dss-demo-webapp/src/test/java/eu/europa/esig/dss/web/service/FOPServiceTest.java
@@ -1,10 +1,9 @@
 package eu.europa.esig.dss.web.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
@@ -16,12 +15,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.w3c.dom.Document;
 
-import eu.europa.esig.dss.DSSXMLUtils;
 import eu.europa.esig.dss.jaxb.detailedreport.DetailedReport;
 import eu.europa.esig.dss.jaxb.simplereport.SimpleReport;
-import eu.europa.esig.dss.validation.reports.Reports;
 
 @ContextConfiguration("/spring/applicationContext.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -32,14 +28,13 @@ public class FOPServiceTest {
 
 	@Test
 	public void generateSimpleReport() throws Exception {
-		JAXBContext context = JAXBContext
-				.newInstance(SimpleReport.class.getPackage().getName());
+		JAXBContext context = JAXBContext.newInstance(SimpleReport.class.getPackage().getName());
 		Unmarshaller unmarshaller = context.createUnmarshaller();
 		Marshaller marshaller = context.createMarshaller();
 
 		SimpleReport simpleReport = (SimpleReport) unmarshaller.unmarshal(new File("src/test/resources/simpleReport.xml"));
 		assertNotNull(simpleReport);
-		
+
 		StringWriter writer = new StringWriter();
 		marshaller.marshal(simpleReport, writer);
 
@@ -49,14 +44,13 @@ public class FOPServiceTest {
 
 	@Test
 	public void generateDetailedReport() throws Exception {
-		JAXBContext context = JAXBContext
-				.newInstance(DetailedReport.class.getPackage().getName());
+		JAXBContext context = JAXBContext.newInstance(DetailedReport.class.getPackage().getName());
 		Unmarshaller unmarshaller = context.createUnmarshaller();
 		Marshaller marshaller = context.createMarshaller();
 
 		DetailedReport detailedReport = (DetailedReport) unmarshaller.unmarshal(new File("src/test/resources/detailedReport.xml"));
 		assertNotNull(detailedReport);
-		
+
 		StringWriter writer = new StringWriter();
 		marshaller.marshal(detailedReport, writer);
 
