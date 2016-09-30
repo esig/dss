3667e361ae6b9a0cd653b17d49569d3ce9023b19
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2016-09-30 14:38:19 +0200

    Fix unit test JDK7/8 (PKCS#12 + JDK7 -> keystore (cert + private key))

--- dss-spi/src/test/java/eu/europa/esig/dss/x509/KeyStoreCertificateSourceTest.java
+++ dss-spi/src/test/java/eu/europa/esig/dss/x509/KeyStoreCertificateSourceTest.java
@@ -4,7 +4,11 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import eu.europa.esig.dss.DSSException;
@@ -14,8 +18,14 @@ import eu.europa.esig.dss.utils.Utils;
 public class KeyStoreCertificateSourceTest {
 
 	private static final String KEYSTORE_PASSWORD = "dss-password";
-	private static final String KEYSTORE_TYPE = "PKCS12";
-	private static final String KEYSTORE_FILEPATH = "src/test/resources/keystore.p12";
+	private static final String KEYSTORE_TYPE = "JKS";
+	private static final String ORIGINAL_KEYSTORE_FILEPATH = "src/test/resources/keystore.jks";
+	private static final String KEYSTORE_FILEPATH = "target/keystore.jks";
+
+	@Before
+	public void init() throws IOException {
+		Utils.copy(new FileInputStream(ORIGINAL_KEYSTORE_FILEPATH), new FileOutputStream(KEYSTORE_FILEPATH));
+	}
 
 	@Test
 	public void testLoadAddAndDelete() {
