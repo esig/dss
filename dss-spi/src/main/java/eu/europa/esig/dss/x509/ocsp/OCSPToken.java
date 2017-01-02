528dd217cb57f8d8a7c1ee90d89f90edd7692a89
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2017-01-02 13:53:00 +0100

    Avoid exception in case of mal formed OCSP response

--- dss-spi/src/main/java/eu/europa/esig/dss/x509/ocsp/OCSPToken.java
+++ dss-spi/src/main/java/eu/europa/esig/dss/x509/ocsp/OCSPToken.java
@@ -86,8 +86,7 @@ public class OCSPToken extends RevocationToken {
 		this.extraInfo = new TokenValidationExtraInfo();
 	}
 
-	public boolean extractInfo() {
-
+	public void extractInfo() {
 		if (basicOCSPResp != null) {
 			this.productionDate = basicOCSPResp.getProducedAt();
 			this.signatureAlgorithm = SignatureAlgorithm.forOID(basicOCSPResp.getSignatureAlgOID().getId());
@@ -100,14 +99,13 @@ public class OCSPToken extends RevocationToken {
 				extractStatusInfo(bestSingleResp);
 			}
 		}
-
-		return true;
 	}
 
 	private SingleResp getBestSingleResp(final BasicOCSPResp basicOCSPResp, final CertificateID certId) {
 		Date bestUpdate = null;
 		SingleResp bestSingleResp = null;
-		for (final SingleResp singleResp : basicOCSPResp.getResponses()) {
+		SingleResp[] responses = getResponses(basicOCSPResp);
+		for (final SingleResp singleResp : responses) {
 			if (DSSRevocationUtils.matches(certId, singleResp)) {
 				final Date thisUpdate = singleResp.getThisUpdate();
 				if ((bestUpdate == null) || thisUpdate.after(bestUpdate)) {
@@ -117,6 +115,17 @@ public class OCSPToken extends RevocationToken {
 			}
 		}
 		return bestSingleResp;
+	}
+
+	private SingleResp[] getResponses(final BasicOCSPResp basicOCSPResp) {
+		SingleResp[] responses = new SingleResp[] {};
+		try {
+			responses = basicOCSPResp.getResponses();
+		} catch (Exception e) {
+			logger.error("Unable to parse the responses object from OCSP", e);
+			extraInfo.infoOCSPException("Unable to parse the responses object from OCSP : " + e.getMessage());
+		}
+		return responses;
 	}
 
 	private void extractStatusInfo(SingleResp bestSingleResp) {
