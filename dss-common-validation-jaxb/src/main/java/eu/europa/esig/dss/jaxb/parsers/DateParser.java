753f520dc873a689ab636212c1bf82480d5e44d3
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2017-03-20 15:38:01 +0100

    Use UTC

--- dss-common-validation-jaxb/src/main/java/eu/europa/esig/dss/jaxb/parsers/DateParser.java
+++ dss-common-validation-jaxb/src/main/java/eu/europa/esig/dss/jaxb/parsers/DateParser.java
@@ -2,6 +2,7 @@ package eu.europa.esig.dss.jaxb.parsers;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +11,14 @@ public class DateParser {
 
 	private static final Logger logger = LoggerFactory.getLogger(DateParser.class);
 
-	private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";// "dd/MM/yyyy HH:mm:ss";
+	private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+	private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
 
 	public static Date parse(String v) {
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+			sdf.setTimeZone(UTC);
 			sdf.setLenient(false);
 			return sdf.parse(v);
 		} catch (Exception e) {
@@ -26,6 +30,7 @@ public class DateParser {
 	public static String print(Date v) {
 		if (v != null) {
 			SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+			sdf.setTimeZone(UTC);
 			return sdf.format(v);
 		}
 		return null;
