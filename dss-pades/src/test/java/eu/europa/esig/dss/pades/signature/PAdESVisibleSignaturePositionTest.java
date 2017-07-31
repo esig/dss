9a096033c2d4ab14becf4f13afe0d8db6e3fc92a
Fatér Zsolt <fater.zsolt@gmail.com>
2017-07-31 16:29:34 +0200

    Rotation support for visual signature - Pull request #71

--- dss-pades/src/test/java/eu/europa/esig/dss/pades/signature/PAdESVisibleSignaturePositionTest.java
+++ dss-pades/src/test/java/eu/europa/esig/dss/pades/signature/PAdESVisibleSignaturePositionTest.java
@@ -43,6 +43,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.awt.Color;
 import java.awt.Font;
