e303085236573ae1da2c4424c82c2f9df79fc004
Fatér Zsolt <fater.zsolt@gmail.com>
2017-09-19 10:19:03 +0200

    CommonDataLoader proxy NTLM authentication support
    - ssl connection key store support
    - truststore default: use java default truststore

--- dss-service/src/main/java/eu/europa/esig/dss/client/http/commons/NativeCommonsDataLoader.java
+++ dss-service/src/main/java/eu/europa/esig/dss/client/http/commons/NativeCommonsDataLoader.java
@@ -102,6 +102,7 @@ public class NativeCommonsDataLoader implements DataLoader {
     private String sslTruststorePath;
     private String sslTruststoreType;
     private String sslTruststorePassword;
+    private boolean sslTruststoreTrustEverything = false;
 
     /**
      * The default constructor for CommonsDataLoader.
@@ -741,6 +742,14 @@ public class NativeCommonsDataLoader implements DataLoader {
         this.sslTruststorePassword = sslTruststorePassword;
     }
 
+    public boolean isSslTruststoreTrustEverything() {
+        return sslTruststoreTrustEverything;
+    }
+
+    public void setSslTruststoreTrustEverything(boolean sslTruststoreTrustEverything) {
+        this.sslTruststoreTrustEverything = sslTruststoreTrustEverything;
+    }
+
     private TrustManager[] createTrustManagers() {
         try {
             if (StringUtils.isNotBlank(sslTruststorePath) && StringUtils.isNotBlank(sslTruststoreType) && StringUtils.isNotBlank(sslTruststorePassword)) {
@@ -749,8 +758,10 @@ public class NativeCommonsDataLoader implements DataLoader {
                 TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
                 trustManagerFactory.init(trustKeyStore);
                 return trustManagerFactory.getTrustManagers();
-            } else {
+            } else if(sslTruststoreTrustEverything) {
                 return new TrustManager[]{new AcceptAllTrustManager()};
+            } else {
+                return null;
             }
         } catch (Exception e) {
             throw new DSSException(e);
