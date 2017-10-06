5a39af5ffdaaabb1ad9378acb453597a35a6bc9d
Davyd Santos <davydsantos@users.noreply.github.com>
2017-10-06 11:15:26 -0700

    Reverting changes ...

--- dss-validation-soap/src/main/java/eu/europa/esig/dss/validation/SoapDocumentValidationServiceImpl.java
+++ dss-validation-soap/src/main/java/eu/europa/esig/dss/validation/SoapDocumentValidationServiceImpl.java
@@ -1,14 +1,9 @@
 package eu.europa.esig.dss.validation;
 
-import javax.jws.WebParam;
-import javax.jws.WebResult;
-import javax.jws.WebService;
-
-import eu.europa.esig.dss.validation.reports.dto.DataToValidateDTO;
+import eu.europa.esig.dss.DataToValidateDTO;
 import eu.europa.esig.dss.validation.reports.dto.ReportsDTO;
 
 @SuppressWarnings("serial")
-@WebService(serviceName = "DocumentValidationService", targetNamespace = "http://validation.dss.esig.europa.eu/", portName = "soap")
 public class SoapDocumentValidationServiceImpl implements SoapDocumentValidationService {
 
 	private RemoteDocumentValidationService validationService;
@@ -18,8 +13,7 @@ public class SoapDocumentValidationServiceImpl implements SoapDocumentValidation
 	}
 
 	@Override
-	@WebResult(name = "WSReportsDTO")
-	public WSReportsDTO validateSignature(@WebParam(name = "dataToValidateDTO") DataToValidateDTO dataToValidate) {
+	public WSReportsDTO validateSignature(DataToValidateDTO dataToValidate) {
 		ReportsDTO reportsDTO = validationService.validateDocument(dataToValidate.getSignedDocument(), dataToValidate.getOriginalDocument(),
 				dataToValidate.getPolicy());
 		return new WSReportsDTO(reportsDTO.getDiagnosticData(), reportsDTO.getSimpleReport(), reportsDTO.getDetailedReport());
