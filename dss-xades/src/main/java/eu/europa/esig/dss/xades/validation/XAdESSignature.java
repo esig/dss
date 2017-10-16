21f99f5d0d7cf3d15f15e93fd94d5318f7856076
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2017-10-16 08:26:37 +0200

    DSS-1266 : ECC based on brainpool not supported

--- dss-xades/src/main/java/eu/europa/esig/dss/xades/validation/XAdESSignature.java
+++ dss-xades/src/main/java/eu/europa/esig/dss/xades/validation/XAdESSignature.java
@@ -55,6 +55,7 @@ import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.x509.GeneralName;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -153,6 +154,7 @@ public class XAdESSignature extends DefaultAdvancedSignature {
 	static {
 
 		Init.init();
+		JCEMapper.setProviderId(BouncyCastleProvider.PROVIDER_NAME);
 
 		/**
 		 * Adds the support of ECDSA_RIPEMD160 for XML signature. Used by AT. The BC provider must be previously added.
