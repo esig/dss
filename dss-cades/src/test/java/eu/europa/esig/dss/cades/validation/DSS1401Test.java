2c8e6d1728a45ca5f334f73de1fbd475861d41a9
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2018-04-04 16:08:08 +0200

    DSS-1401 : Add unit tests

--- dss-cades/src/test/java/eu/europa/esig/dss/cades/validation/DSS1401Test.java
+++ dss-cades/src/test/java/eu/europa/esig/dss/cades/validation/DSS1401Test.java
@@ -2,6 +2,7 @@ package eu.europa.esig.dss.cades.validation;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -32,9 +33,88 @@ public class DSS1401Test {
 		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
 		List<TimestampWrapper> timestamps = signature.getTimestampListByType(TimestampType.ARCHIVE_TIMESTAMP);
 		TimestampWrapper archiveTimestamp = timestamps.get(0);
-		
+
 		assertTrue(archiveTimestamp.isMessageImprintDataIntact());
-		
+
+	}
+
+	@Test
+	public void testFile2() {
+		DSSDocument dssDocument = new FileDocument("src/test/resources/validation/dss-916/test.txt.signed_Certipost-2048.detached.old.p7s");
+		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(dssDocument);
+		validator.setCertificateVerifier(new CommonCertificateVerifier());
+		validator.setDetachedContents(Arrays.<DSSDocument> asList(new FileDocument("src/test/resources/validation/dss-916/test.txt")));
+		Reports reports = validator.validateDocument();
+
+		reports.print();
+
+		DiagnosticData diagnosticData = reports.getDiagnosticData();
+
+		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
+		List<TimestampWrapper> timestamps = signature.getTimestampListByType(TimestampType.ARCHIVE_TIMESTAMP);
+		TimestampWrapper archiveTimestamp = timestamps.get(0);
+
+		assertTrue(archiveTimestamp.isMessageImprintDataIntact());
+
+	}
+
+	@Test
+	public void testFile3() {
+		DSSDocument dssDocument = new FileDocument("src/test/resources/validation/dss-916/test.txt.signed.qes.attached.p7s");
+		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(dssDocument);
+		validator.setCertificateVerifier(new CommonCertificateVerifier());
+		Reports reports = validator.validateDocument();
+
+		reports.print();
+
+		DiagnosticData diagnosticData = reports.getDiagnosticData();
+
+		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
+		List<TimestampWrapper> timestamps = signature.getTimestampListByType(TimestampType.ARCHIVE_TIMESTAMP);
+		TimestampWrapper archiveTimestamp = timestamps.get(0);
+
+		assertTrue(archiveTimestamp.isMessageImprintDataIntact());
+
+	}
+
+	@Test
+	public void testFile4() {
+		DSSDocument dssDocument = new FileDocument("src/test/resources/validation/dss-916/test.txt.signed.qes.detached.p7s");
+		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(dssDocument);
+		validator.setCertificateVerifier(new CommonCertificateVerifier());
+		validator.setDetachedContents(Arrays.<DSSDocument> asList(new FileDocument("src/test/resources/validation/dss-916/test.txt")));
+		Reports reports = validator.validateDocument();
+
+		reports.print();
+
+		DiagnosticData diagnosticData = reports.getDiagnosticData();
+
+		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
+		List<TimestampWrapper> timestamps = signature.getTimestampListByType(TimestampType.ARCHIVE_TIMESTAMP);
+		TimestampWrapper archiveTimestamp = timestamps.get(0);
+
+		assertTrue(archiveTimestamp.isMessageImprintDataIntact());
+
+	}
+
+	@Test
+	public void testFile5() {
+		DSSDocument dssDocument = new FileDocument("src/test/resources/validation/dss-1344/screenshot.png.signed_qes_detached.p7s");
+		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(dssDocument);
+		validator.setCertificateVerifier(new CommonCertificateVerifier());
+		validator.setDetachedContents(Arrays.<DSSDocument> asList(new FileDocument("src/test/resources/validation/dss-1344/screenshot.png")));
+		Reports reports = validator.validateDocument();
+
+		reports.print();
+
+		DiagnosticData diagnosticData = reports.getDiagnosticData();
+
+		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
+		List<TimestampWrapper> timestamps = signature.getTimestampListByType(TimestampType.ARCHIVE_TIMESTAMP);
+		TimestampWrapper archiveTimestamp = timestamps.get(0);
+
+		assertTrue(archiveTimestamp.isMessageImprintDataIntact());
+
 	}
 
 }
