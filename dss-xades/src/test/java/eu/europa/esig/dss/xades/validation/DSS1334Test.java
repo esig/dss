2acc27bc12b18296068710a1ab406ff14fcbe1f3
Pierrick Vandenbroucke <pierrick.vandenbroucke@nowina.lu>
2018-06-26 09:19:19 +0200

    Refactor

--- dss-xades/src/test/java/eu/europa/esig/dss/xades/validation/DSS1334Test.java
+++ dss-xades/src/test/java/eu/europa/esig/dss/xades/validation/DSS1334Test.java
@@ -3,12 +3,14 @@ package eu.europa.esig.dss.xades.validation;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 import eu.europa.esig.dss.DSSDocument;
 import eu.europa.esig.dss.FileDocument;
 import eu.europa.esig.dss.client.http.IgnoreDataLoader;
+import eu.europa.esig.dss.jaxb.diagnostic.XmlDigestMatcher;
 import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 import eu.europa.esig.dss.validation.SignedDocumentValidator;
 import eu.europa.esig.dss.validation.reports.Reports;
@@ -17,23 +19,12 @@ import eu.europa.esig.dss.validation.reports.wrapper.SignatureWrapper;
 
 public class DSS1334Test {
 
-	private static final String ORIGINAL_FILE = "src/test/resources/validation/dss1334/simple-test.xml";
-
 	@Test
 	public void test1a() {
 		DSSDocument doc = new FileDocument(
 				"src/test/resources/validation/dss1334/document-signed-xades-baseline-b--null-for-filename.xml");
-		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(doc);
-		validator.setDetachedContents(Arrays.<DSSDocument>asList(new FileDocument(ORIGINAL_FILE)));
 
-		CommonCertificateVerifier certificateVerifier = new CommonCertificateVerifier();
-		certificateVerifier.setDataLoader(new IgnoreDataLoader());
-		validator.setCertificateVerifier(certificateVerifier);
-
-		Reports reports = validator.validateDocument();
-		DiagnosticData diagnosticData = reports.getDiagnosticData();
-		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
-		assertTrue(signature.isBLevelTechnicallyValid());
+		assertCryptoValid(doc);
 	}
 
 	@Test
@@ -42,17 +33,8 @@ public class DSS1334Test {
 				"src/test/resources/validation/dss1334/document-signed-xades-baseline-b--null-for-filename.xml");
 		doc.setName(null);
 		doc.setMimeType(null);
-		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(doc);
-		validator.setDetachedContents(Arrays.<DSSDocument>asList(new FileDocument(ORIGINAL_FILE)));
 
-		CommonCertificateVerifier certificateVerifier = new CommonCertificateVerifier();
-		certificateVerifier.setDataLoader(new IgnoreDataLoader());
-		validator.setCertificateVerifier(certificateVerifier);
-
-		Reports reports = validator.validateDocument();
-		DiagnosticData diagnosticData = reports.getDiagnosticData();
-		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
-		assertTrue(signature.isBLevelTechnicallyValid());
+		assertCryptoValid(doc);
 	}
 
 	@Test
@@ -61,51 +43,42 @@ public class DSS1334Test {
 				"src/test/resources/validation/dss1334/document-signed-xades-baseline-b--null-for-filename.xml");
 		doc.setName("");
 		doc.setMimeType(null);
-		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(doc);
-		validator.setDetachedContents(Arrays.<DSSDocument>asList(new FileDocument(ORIGINAL_FILE)));
 
-		CommonCertificateVerifier certificateVerifier = new CommonCertificateVerifier();
-		certificateVerifier.setDataLoader(new IgnoreDataLoader());
-		validator.setCertificateVerifier(certificateVerifier);
-
-		Reports reports = validator.validateDocument();
-		DiagnosticData diagnosticData = reports.getDiagnosticData();
-		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
-		assertTrue(signature.isBLevelTechnicallyValid());
+		assertCryptoValid(doc);
 	}
 
 	@Test
 	public void test2() {
 		DSSDocument doc = new FileDocument(
 				"src/test/resources/validation/dss1334/simple-test-signed-xades-baseline-b.xml");
-		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(doc);
-		validator.setDetachedContents(Arrays.<DSSDocument>asList(new FileDocument(ORIGINAL_FILE)));
 
-		CommonCertificateVerifier certificateVerifier = new CommonCertificateVerifier();
-		certificateVerifier.setDataLoader(new IgnoreDataLoader());
-		validator.setCertificateVerifier(certificateVerifier);
-
-		Reports reports = validator.validateDocument();
-		DiagnosticData diagnosticData = reports.getDiagnosticData();
-		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
-		assertTrue(signature.isBLevelTechnicallyValid());
+		assertCryptoValid(doc);
 	}
 
 	@Test
 	public void test3() {
 		DSSDocument doc = new FileDocument(
 				"src/test/resources/validation/dss1334/simple-test.signed-only-detached-LuxTrustCA3.xml");
-		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(doc);
-		validator.setDetachedContents(Arrays.<DSSDocument>asList(new FileDocument(ORIGINAL_FILE)));
+		assertCryptoValid(doc);
+	}
 
+	private void assertCryptoValid(DSSDocument doc) {
+		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(doc);
 		CommonCertificateVerifier certificateVerifier = new CommonCertificateVerifier();
 		certificateVerifier.setDataLoader(new IgnoreDataLoader());
 		validator.setCertificateVerifier(certificateVerifier);
+		validator.setDetachedContents(
+				Arrays.<DSSDocument>asList(new FileDocument("src/test/resources/validation/dss1334/simple-test.xml")));
 
 		Reports reports = validator.validateDocument();
 		DiagnosticData diagnosticData = reports.getDiagnosticData();
 		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
 		assertTrue(signature.isBLevelTechnicallyValid());
+		List<XmlDigestMatcher> digestMatchers = signature.getDigestMatchers();
+		for (XmlDigestMatcher digestMatcher : digestMatchers) {
+			assertTrue(digestMatcher.isDataFound());
+			assertTrue(digestMatcher.isDataIntact());
+		}
 	}
 
 }
