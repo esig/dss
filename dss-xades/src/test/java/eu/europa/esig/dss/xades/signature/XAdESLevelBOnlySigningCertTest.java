4e1d4d1563f366fae397f6cf5368edb6f9c8f784
Pierrick Vandenbroucke <pierrick.vandenbroucke@nowina.lu>
2018-08-09 13:18:28 +0200

    Add unit test to ensure that DSS is able to fully validate a signature which only contains  the signing certificate

---
+++ dss-xades/src/test/java/eu/europa/esig/dss/xades/signature/XAdESLevelBOnlySigningCertTest.java
@@ -0,0 +1,95 @@
+/**
+ * DSS - Digital Signature Services
+ * Copyright (C) 2015 European Commission, provided under the CEF programme
+ *
+ * This file is part of the "DSS - Digital Signature Services" project.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package eu.europa.esig.dss.xades.signature;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Before;
+
+import eu.europa.esig.dss.DSSDocument;
+import eu.europa.esig.dss.DomUtils;
+import eu.europa.esig.dss.FileDocument;
+import eu.europa.esig.dss.SignatureLevel;
+import eu.europa.esig.dss.SignaturePackaging;
+import eu.europa.esig.dss.signature.DocumentSignatureService;
+import eu.europa.esig.dss.validation.policy.rules.Indication;
+import eu.europa.esig.dss.validation.reports.SimpleReport;
+import eu.europa.esig.dss.x509.CertificateToken;
+import eu.europa.esig.dss.xades.XAdESSignatureParameters;
+import eu.europa.esig.dss.xades.validation.XAdESSignature;
+
+public class XAdESLevelBOnlySigningCertTest extends AbstractXAdESTestSignature {
+
+	private DocumentSignatureService<XAdESSignatureParameters> service;
+	private XAdESSignatureParameters signatureParameters;
+	private DSSDocument documentToSign;
+
+	@Before
+	public void init() throws Exception {
+		documentToSign = new FileDocument(new File("src/test/resources/sample.xml"));
+
+		signatureParameters = new XAdESSignatureParameters();
+		signatureParameters.setSigningCertificate(getSigningCert());
+		// Cert chain omitted
+		signatureParameters.setSignaturePackaging(SignaturePackaging.ENVELOPING);
+		signatureParameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_B);
+
+		service = new XAdESService(getCompleteCertificateVerifier());
+	}
+
+	@Override
+	protected void onDocumentSigned(byte[] byteArray) {
+		XAdESSignature sig = new XAdESSignature(DomUtils.buildDOM(byteArray).getDocumentElement());
+		List<CertificateToken> certificates = sig.getCertificates();
+		assertEquals(1, certificates.size());
+	}
+
+	@Override
+	protected String getSigningAlias() {
+		return GOOD_USER;
+	}
+
+	@Override
+	protected DocumentSignatureService<XAdESSignatureParameters> getService() {
+		return service;
+	}
+
+	@Override
+	protected XAdESSignatureParameters getSignatureParameters() {
+		return signatureParameters;
+	}
+
+	@Override
+	protected DSSDocument getDocumentToSign() {
+		return documentToSign;
+	}
+
+	@Override
+	protected void verifySimpleReport(SimpleReport simpleReport) {
+		super.verifySimpleReport(simpleReport);
+		
+		assertEquals(Indication.TOTAL_PASSED, simpleReport.getIndication(simpleReport.getFirstSignatureId()));
+	}
+
+}
