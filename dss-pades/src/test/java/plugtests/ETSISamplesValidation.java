29df9003c011c59790fdef0e74b8f1c33a0c0ee4
Pierrick Vandenbroucke <pierrick.vandenbroucke@nowina.lu>
2018-08-28 08:48:45 +0200

    Improve unit tests to cover both implementations

--- dss-pades/src/test/java/plugtests/ETSISamplesValidation.java
+++ dss-pades/src/test/java/plugtests/ETSISamplesValidation.java
@@ -2,7 +2,9 @@ package plugtests;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -11,9 +13,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import eu.europa.esig.dss.FileDocument;
+import eu.europa.esig.dss.DSSDocument;
+import eu.europa.esig.dss.InMemoryDocument;
 import eu.europa.esig.dss.client.http.IgnoreDataLoader;
-import eu.europa.esig.dss.utils.Utils;
 import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 import eu.europa.esig.dss.validation.SignedDocumentValidator;
 import eu.europa.esig.dss.validation.reports.DetailedReport;
@@ -27,26 +29,33 @@ import eu.europa.esig.dss.validation.reports.wrapper.DiagnosticData;
 @RunWith(Parameterized.class)
 public class ETSISamplesValidation {
 
+
 	@Parameters(name = "Validation {index} : {0}")
-	public static Collection<Object[]> data() {
-		File folder = new File("src/test/resources/plugtest");
-		Collection<File> listFiles = Utils.listFiles(folder, new String[] { "pdf" }, true);
+	public static Collection<Object[]> data() throws IOException {
+
+		// We use this file because File.listFiles() doesn't work from another jar
+		String listFiles = "/plugtest/plugtest_files.txt";
+
 		Collection<Object[]> dataToRun = new ArrayList<Object[]>();
-		for (File file : listFiles) {
-			dataToRun.add(new Object[] { file });
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(ETSISamplesValidation.class.getResourceAsStream(listFiles)))) {
+			String filepath;
+			while ((filepath = br.readLine()) != null) {
+				dataToRun.add(new Object[] { new InMemoryDocument(ETSISamplesValidation.class.getResourceAsStream(filepath)) });
+			}
+
 		}
 		return dataToRun;
 	}
 
-	private File fileToTest;
+	private DSSDocument doc;
 
-	public ETSISamplesValidation(File fileToTest) {
-		this.fileToTest = fileToTest;
+	public ETSISamplesValidation(DSSDocument doc) {
+		this.doc = doc;
 	}
 
 	@Test
 	public void testValidate() {
-		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(new FileDocument(fileToTest));
+		SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(doc);
 
 		CommonCertificateVerifier certificateVerifier = new CommonCertificateVerifier();
 		certificateVerifier.setDataLoader(new IgnoreDataLoader());
