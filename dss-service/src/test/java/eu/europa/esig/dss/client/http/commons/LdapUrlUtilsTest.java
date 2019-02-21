b375c6ed17bb3643d1e6a8f99ee3fc2859893a83
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2019-02-21 08:42:43 +0100

    added slovakian urls to encoder test

--- dss-service/src/test/java/eu/europa/esig/dss/client/http/commons/LdapUrlUtilsTest.java
+++ dss-service/src/test/java/eu/europa/esig/dss/client/http/commons/LdapUrlUtilsTest.java
@@ -15,6 +15,10 @@ public class LdapUrlUtilsTest {
 	    assertEquals("ldap://crl-source.hn/o=%20%20%20", LdapURLUtils.encode("ldap://crl-source.hn/o=   "));
 	    assertEquals("ldap://crl-source.hn/o=Hello%20+%20World;", LdapURLUtils.encode("ldap://crl-source.hn/o=Hello + World;"));
 	    assertEquals("ldap://crl-source.hn/o=Hello%20+%20World;", LdapURLUtils.encode("ldap://crl-source.hn/o=Hello%20+%20World;"));
+	    assertEquals("ldap://ep.nbusr.sk/cn%3dKCA%20NBU%20SR%203,ou%3dSIBEP,o%3dNarodny%20bezpecnostny%20urad,l%3dBratislava,c%3dSK?certificateRevocationList", 
+	    		LdapURLUtils.encode("ldap://ep.nbusr.sk/cn%3dKCA%20NBU%20SR%203,ou%3dSIBEP,o%3dNarodny%20bezpecnostny%20urad,l%3dBratislava,c%3dSK?certificateRevocationList"));
+	    assertEquals("ldap:///cn%3dKCA%20NBU%20SR%203,ou%3dSIBEP,o%3dNarodny%20bezpecnostny%20urad,l%3dBratislava,c%3dSK?certificateRevocationList", 
+	    		LdapURLUtils.encode("ldap:///cn%3dKCA%20NBU%20SR%203,ou%3dSIBEP,o%3dNarodny%20bezpecnostny%20urad,l%3dBratislava,c%3dSK?certificateRevocationList"));
 	}
 
 }
