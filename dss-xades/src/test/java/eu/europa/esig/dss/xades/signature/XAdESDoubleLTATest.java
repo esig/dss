f058e1ff8cdaa97e4e84ba800c5e7d9d41e80a2a
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2019-07-01 14:17:48 +0200

    DSS-1688 added test cases to ensure that generation of validation data is complete

---
+++ dss-xades/src/test/java/eu/europa/esig/dss/xades/signature/XAdESDoubleLTATest.java
@@ -0,0 +1,94 @@
+package eu.europa.esig.dss.xades.signature;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+import eu.europa.esig.dss.DSSDocument;
+import eu.europa.esig.dss.FileDocument;
+import eu.europa.esig.dss.SignatureLevel;
+import eu.europa.esig.dss.SignaturePackaging;
+import eu.europa.esig.dss.SignatureValue;
+import eu.europa.esig.dss.ToBeSigned;
+import eu.europa.esig.dss.signature.PKIFactoryAccess;
+import eu.europa.esig.dss.validation.SignedDocumentValidator;
+import eu.europa.esig.dss.validation.policy.rules.Indication;
+import eu.europa.esig.dss.validation.reports.DetailedReport;
+import eu.europa.esig.dss.validation.reports.Reports;
+import eu.europa.esig.dss.validation.reports.SimpleReport;
+import eu.europa.esig.dss.validation.reports.wrapper.CertificateWrapper;
+import eu.europa.esig.dss.validation.reports.wrapper.DiagnosticData;
+import eu.europa.esig.dss.xades.XAdESSignatureParameters;
+
+public class XAdESDoubleLTATest extends PKIFactoryAccess {
+	
+	@Test
+	public void test() throws IOException {
+		DSSDocument documentToSign = new FileDocument("src/test/resources/sample.xml");
+
+        XAdESSignatureParameters signatureParameters = new XAdESSignatureParameters();
+        signatureParameters.bLevel().setSigningDate(new Date());
+        signatureParameters.setSigningCertificate(getSigningCert());
+        signatureParameters.setCertificateChain(getCertificateChain());
+        signatureParameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_LT);
+		signatureParameters.setSignaturePackaging(SignaturePackaging.DETACHED);
+
+        XAdESService service = new XAdESService(getCompleteCertificateVerifier());
+        service.setTspSource(getGoodTsa());
+
+        ToBeSigned dataToSign = service.getDataToSign(documentToSign, signatureParameters);
+        SignatureValue signatureValue = getToken().sign(dataToSign, signatureParameters.getDigestAlgorithm(), getPrivateKeyEntry());
+        DSSDocument signedDocument = service.signDocument(documentToSign, signatureParameters, signatureValue);
+
+        // signedDocument.save("target/signed.xml");
+
+        service.setTspSource(getGoodTsaCrossCertification());
+
+        XAdESSignatureParameters extendParameters = new XAdESSignatureParameters();
+        extendParameters.setDetachedContents(Arrays.asList(documentToSign));
+        extendParameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_LTA);
+        DSSDocument extendedDocument = service.extendDocument(signedDocument, extendParameters);
+        
+        DSSDocument doubleLTADoc = service.extendDocument(extendedDocument, extendParameters);
+        
+        // doubleLTADoc.save("target/doubleLTA.xml");
+        
+        SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(doubleLTADoc);
+        validator.setCertificateVerifier(getOfflineCertificateVerifier());
+        validator.setDetachedContents(Arrays.asList(documentToSign));
+        Reports reports = validator.validateDocument();
+        
+        // reports.print();
+        
+        SimpleReport simpleReport = reports.getSimpleReport();
+        assertEquals(Indication.TOTAL_PASSED, simpleReport.getIndication(simpleReport.getFirstSignatureId()));
+        
+        DetailedReport detailedReport = reports.getDetailedReport();
+        List<String> timestampIds = detailedReport.getTimestampIds();
+        assertEquals(3, timestampIds.size());
+        for (String id : timestampIds) {
+            assertEquals(Indication.PASSED, detailedReport.getTimestampValidationIndication(id));
+        }
+        
+        DiagnosticData diagnosticData = reports.getDiagnosticData();
+        
+        List<CertificateWrapper> usedCertificates = diagnosticData.getUsedCertificates();
+        for (CertificateWrapper certificate : usedCertificates) {
+        	assertTrue("Certificate with id : [" + certificate.getId() + "] does not have a revocation data!", 
+        			certificate.isRevocationDataAvailable() || certificate.isTrusted() || certificate.isSelfSigned());
+        }
+        
+	}
+
+	@Override
+	protected String getSigningAlias() {
+		return RSA_SHA3_USER;
+	}
+
+}
