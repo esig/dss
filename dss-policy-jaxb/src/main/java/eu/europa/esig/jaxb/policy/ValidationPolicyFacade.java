bb2bbc881c12644f0706ade8f9769337e610b598
Pierrick Vandenbroucke <pierrick.vandenbroucke@nowina.lu>
2019-07-02 08:55:22 +0200

    Clean ValidationResourceManager

--- dss-policy-jaxb/src/main/java/eu/europa/esig/jaxb/policy/ValidationPolicyFacade.java
+++ dss-policy-jaxb/src/main/java/eu/europa/esig/jaxb/policy/ValidationPolicyFacade.java
@@ -2,12 +2,14 @@ package eu.europa.esig.jaxb.policy;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 
 import org.xml.sax.SAXException;
@@ -21,19 +23,29 @@ public class ValidationPolicyFacade {
 	}
 
 	public ConstraintsParameters unmarshall(File file) throws JAXBException, XMLStreamException, IOException, SAXException {
+		Unmarshaller unmarshaller = getUnmarshaller();
 
+		return (ConstraintsParameters) unmarshaller.unmarshal(avoidXXE(new StreamSource(file)));
+	}
+
+	public ConstraintsParameters unmarshall(InputStream inputStream) throws JAXBException, XMLStreamException, IOException, SAXException {
+		Unmarshaller unmarshaller = getUnmarshaller();
+
+		return (ConstraintsParameters) unmarshaller.unmarshal(avoidXXE(new StreamSource(inputStream)));
+	}
+
+	private Unmarshaller getUnmarshaller() throws JAXBException, IOException, SAXException {
 		MarshallerBuilder builder = new MarshallerBuilder(ValidationPolicyXmlDefiner.getJAXBContext(), ValidationPolicyXmlDefiner.getSchema());
 		builder.setValidate(true);
 		Unmarshaller unmarshaller = builder.buildUnmarshaller();
-
-		return (ConstraintsParameters) unmarshaller.unmarshal(avoidXXE(file));
+		return unmarshaller;
 	}
 
-	private XMLStreamReader avoidXXE(File file) throws XMLStreamException {
+	private XMLStreamReader avoidXXE(Source source) throws XMLStreamException {
 		XMLInputFactory xif = XMLInputFactory.newFactory();
 		xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
 		xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
-		return xif.createXMLStreamReader(new StreamSource(file));
+		return xif.createXMLStreamReader(source);
 	}
 
 }
