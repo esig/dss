fde59ca814c4ce1e67624e284f0cbd53f59f75f5
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2019-07-09 10:56:01 +0200

    use cef:dss uri

--- dss-enumerations/src/main/java/eu/europa/esig/dss/enumerations/SignatureQualification.java
+++ dss-enumerations/src/main/java/eu/europa/esig/dss/enumerations/SignatureQualification.java
@@ -28,120 +28,120 @@ public enum SignatureQualification implements UriBasedEnum {
 	/**
 	 * Qualified Electronic Signature
 	 */
-	QESIG("QESig", "Qualified Electronic Signature", "urn:etsi:019102:signatureQualification:QESig"),
+	QESIG("QESig", "Qualified Electronic Signature", "urn:cef:dss:signatureQualification:QESig"),
 
 	/**
 	 * Qualified Electronic Seal
 	 */
-	QESEAL("QESeal", "Qualified Electronic Seal", "urn:etsi:019102:signatureQualification:QESeal"),
+	QESEAL("QESeal", "Qualified Electronic Seal", "urn:cef:dss:signatureQualification:QESeal"),
 
 	/**
 	 * Qualified Electronic Signature or Seal
 	 */
-	QES("QES?", "Qualified Electronic Signature or Seal", "urn:etsi:019102:signatureQualification:QES"),
+	QES("QES?", "Qualified Electronic Signature or Seal", "urn:cef:dss:signatureQualification:QES"),
 
 	/**
 	 * Advanced Electronic Signature supported by a Qualified Certificate
 	 */
-	ADESIG_QC("AdESig-QC", "Advanced Electronic Signature supported by a Qualified Certificate", "urn:etsi:019102:signatureQualification:AdESigQC"),
+	ADESIG_QC("AdESig-QC", "Advanced Electronic Signature supported by a Qualified Certificate", "urn:cef:dss:signatureQualification:AdESigQC"),
 
 	/**
 	 * Advanced Electronic Seal supported by a Qualified Certificate
 	 */
-	ADESEAL_QC("AdESeal-QC", "Advanced Electronic Seal supported by a Qualified Certificate", "urn:etsi:019102:signatureQualification:AdESealQC"),
+	ADESEAL_QC("AdESeal-QC", "Advanced Electronic Seal supported by a Qualified Certificate", "urn:cef:dss:signatureQualification:AdESealQC"),
 
 	/**
 	 * Advanced Electronic Signature or Seal supported by a Qualified Certificate
 	 */
-	ADES_QC("AdES?-QC", "Advanced Electronic Signature or Seal supported by a Qualified Certificate", "urn:etsi:019102:signatureQualification:AdESQC"),
+	ADES_QC("AdES?-QC", "Advanced Electronic Signature or Seal supported by a Qualified Certificate", "urn:cef:dss:signatureQualification:AdESQC"),
 
 	/**
 	 * Advanced Electronic Signature
 	 */
-	ADESIG("AdESig", "Advanced Electronic Signature", "urn:etsi:019102:signatureQualification:AdESig"),
+	ADESIG("AdESig", "Advanced Electronic Signature", "urn:cef:dss:signatureQualification:AdESig"),
 
 	/**
 	 * Advanced Electronic Seal
 	 */
-	ADESEAL("AdESeal", "Advanced Electronic Seal", "urn:etsi:019102:signatureQualification:AdESeal"),
+	ADESEAL("AdESeal", "Advanced Electronic Seal", "urn:cef:dss:signatureQualification:AdESeal"),
 
 	/**
 	 * Advanced Electronic Signature or Seal
 	 */
-	ADES("AdES?", "Advanced Electronic Signature or Seal", "urn:etsi:019102:signatureQualification:AdES"),
+	ADES("AdES?", "Advanced Electronic Signature or Seal", "urn:cef:dss:signatureQualification:AdES"),
 
 	/**
 	 * Indeterminate Qualified Electronic Signature
 	 */
-	INDETERMINATE_QESIG("Indeterminate QESig", "Indeterminate Qualified Electronic Signature", "urn:etsi:019102:signatureQualification:indeterminateQESig"),
+	INDETERMINATE_QESIG("Indeterminate QESig", "Indeterminate Qualified Electronic Signature", "urn:cef:dss:signatureQualification:indeterminateQESig"),
 
 	/**
 	 * Indeterminate Qualified Electronic Seal
 	 */
-	INDETERMINATE_QESEAL("Indeterminate QESeal", "Indeterminate Qualified Electronic Seal", "urn:etsi:019102:signatureQualification:indeterminateQESeal"),
+	INDETERMINATE_QESEAL("Indeterminate QESeal", "Indeterminate Qualified Electronic Seal", "urn:cef:dss:signatureQualification:indeterminateQESeal"),
 
 	/**
 	 * Indeterminate Qualified Electronic Signature or Seal
 	 */
-	INDETERMINATE_QES("Indeterminate QES?", "Indeterminate Qualified Electronic Signature or Seal", "urn:etsi:019102:signatureQualification:indeterminateQES"),
+	INDETERMINATE_QES("Indeterminate QES?", "Indeterminate Qualified Electronic Signature or Seal", "urn:cef:dss:signatureQualification:indeterminateQES"),
 
 	/**
 	 * Indeterminate Advanced Electronic Signature supported by a Qualified Certificate
 	 */
 	INDETERMINATE_ADESIG_QC("Indeterminate AdESig-QC", "Indeterminate Advanced Electronic Signature supported by a Qualified Certificate", 
-			"urn:etsi:019102:signatureQualification:indeterminateAdESigQC"),
+			"urn:cef:dss:signatureQualification:indeterminateAdESigQC"),
 
 	/**
 	 * Indeterminate Advanced Electronic Seal supported by a Qualified Certificate
 	 */
 	INDETERMINATE_ADESEAL_QC("Indeterminate AdESeal-QC", "Indeterminate Advanced Electronic Seal supported by a Qualified Certificate", 
-			"urn:etsi:019102:signatureQualification:indeterminateAdESealQC"),
+			"urn:cef:dss:signatureQualification:indeterminateAdESealQC"),
 
 	/**
 	 * Indeterminate Advanced Electronic Signature or Seal supported by a Qualified Certificate
 	 */
 	INDETERMINATE_ADES_QC("Indeterminate AdES?-QC", "Indeterminate Advanced Electronic Signature or Seal supported by a Qualified Certificate", 
-			"urn:etsi:019102:signatureQualification:indeterminateAdESQC"),
+			"urn:cef:dss:signatureQualification:indeterminateAdESQC"),
 
 	/**
 	 * Indeterminate Advanced Electronic Signature
 	 */
 	INDETERMINATE_ADESIG("Indeterminate AdESig", "Indeterminate Advanced Electronic Signature", 
-			"urn:etsi:019102:signatureQualification:indeterminateAdESig"),
+			"urn:cef:dss:signatureQualification:indeterminateAdESig"),
 
 	/**
 	 * Indeterminate Advanced Electronic Seal
 	 */
 	INDETERMINATE_ADESEAL("Indeterminate AdESeal", "Indeterminate Advanced Electronic Seal", 
-			"urn:etsi:019102:signatureQualification:indeterminateAdESeal"),
+			"urn:cef:dss:signatureQualification:indeterminateAdESeal"),
 
 	/**
 	 * Indeterminate Advanced Electronic Signature or Seal
 	 */
 	INDETERMINATE_ADES("Indeterminate AdES?", "Indeterminate Advanced Electronic Signature or Seal", 
-			"urn:etsi:019102:signatureQualification:indeterminateAdES"),
+			"urn:cef:dss:signatureQualification:indeterminateAdES"),
 
 	/**
 	 * Not Advanced Electronic Signature but supported by a Qualified Certificate
 	 */
 	NOT_ADES_QC_QSCD("Not AdES but QC with QSCD", "Not Advanced Electronic Signature but supported by a Qualified Certificate", 
-	"urn:etsi:019102:signatureQualification:notAdESbutQCwithQSCD"),
+	"urn:cef:dss:signatureQualification:notAdESbutQCwithQSCD"),
 
 	/**
 	 * Not Advanced Electronic Signature but supported by a Qualified Certificate
 	 */
 	NOT_ADES_QC("Not AdES but QC", "Not Advanced Electronic Signature but supported by a Qualified Certificate", 
-			"urn:etsi:019102:signatureQualification:notAdESbutQC"),
+			"urn:cef:dss:signatureQualification:notAdESbutQC"),
 
 	/**
 	 * Not Advanced Electronic Signature
 	 */
-	NOT_ADES("Not AdES", "Not Advanced Electronic Signature", "urn:etsi:019102:signatureQualification:notAdES"),
+	NOT_ADES("Not AdES", "Not Advanced Electronic Signature", "urn:cef:dss:signatureQualification:notAdES"),
 
 	/**
 	 * Not Applicable
 	 */
-	NA("N/A", "Not applicable", "urn:etsi:019102:signatureQualification:notApplicable");
+	NA("N/A", "Not applicable", "urn:cef:dss:signatureQualification:notApplicable");
 
 	private static class Registry {
 
