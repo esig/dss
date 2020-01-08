b8143ce38548f936e3198583e9238c2b2cbe00a2
a.koepe <adrian.koepe.ext@openlimit.com>
2020-01-08 15:09:32 +0100

    Added missing/forgotten import

--- dss-document/src/test/java/eu/europa/esig/dss/validation/TimestampValidatorTest.java
+++ dss-document/src/test/java/eu/europa/esig/dss/validation/TimestampValidatorTest.java
@@ -2,6 +2,7 @@ package eu.europa.esig.dss.validation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
