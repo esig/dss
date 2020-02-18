2d5a4065b202f30fac9942f800504eb41d95912d
Pierrick Vandenbroucke <pierrick.vandenbroucke@nowina.lu>
2020-02-18 15:13:55 +0100

    DSS-1975 : Signature validation JAdES Baseline profile B

--- dss-jades/src/main/java/eu/europa/esig/dss/jades/validation/CustomJsonWebSignature.java
+++ dss-jades/src/main/java/eu/europa/esig/dss/jades/validation/CustomJsonWebSignature.java
@@ -1,14 +1,31 @@
 package eu.europa.esig.dss.jades.validation;
 
+import java.util.Objects;
+
 import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.lang.JoseException;
+
+import eu.europa.esig.dss.model.DSSException;
 
 public class CustomJsonWebSignature extends JsonWebSignature {
-	
+
+	public CustomJsonWebSignature(String header, String payload, String signature) {
+		Objects.requireNonNull(header, "Header part cannot be null");
+		Objects.requireNonNull(payload, "Payload part cannot be null");
+		Objects.requireNonNull(signature, "Signature part cannot be null");
+
+		try {
+			setCompactSerializationParts(new String[] { header, payload, signature });
+		} catch (JoseException e) {
+			throw new DSSException("Unable to instantiate a compact JWS", e);
+		}
+	}
+
 	@Override
 	public String getEncodedHeader() {
 		return super.getEncodedHeader();
 	}
-	
+
 	public byte[] getSignatureValue() {
 		return super.getSignature();
 	}
---
+++ dss-jades/src/main/java/eu/europa/esig/dss/jades/validation/JAdESAttribute.java
@@ -0,0 +1,19 @@
+package eu.europa.esig.dss.jades.validation;
+
+import eu.europa.esig.dss.validation.ISignatureAttribute;
+
+public class JAdESAttribute implements ISignatureAttribute {
+
+	private final String key;
+	private final Object value;
+
+	public JAdESAttribute(String key, Object value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public String getHeaderName() {
+		return key;
+	}
+
+}
