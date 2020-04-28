dd9a7bdd4bfefed750fde3aa75330d35783855f7
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2020-04-28 14:34:38 +0200

    Add null check (cv can be null in case of DocumentValidator.getSignatures())

--- dss-document/src/main/java/eu/europa/esig/dss/validation/CertificateVerifierBuilder.java
+++ dss-document/src/main/java/eu/europa/esig/dss/validation/CertificateVerifierBuilder.java
@@ -15,31 +15,35 @@ public class CertificateVerifierBuilder {
 
 	public CertificateVerifier buildCompleteCopy() {
 		CertificateVerifier copy = new CommonCertificateVerifier(true);
-		copy.setDefaultDigestAlgorithm(certificateVerifier.getDefaultDigestAlgorithm());
-		copy.setDataLoader(certificateVerifier.getDataLoader());
-		copy.setCrlSource(certificateVerifier.getCrlSource());
-		copy.setOcspSource(certificateVerifier.getOcspSource());
-		copy.setCheckRevocationForUntrustedChains(certificateVerifier.isCheckRevocationForUntrustedChains());
-		copy.setAdjunctCertSource(certificateVerifier.getAdjunctCertSource());
-		List<CertificateSource> trustedCertSources = certificateVerifier.getTrustedCertSources();
-		for (CertificateSource certificateSource : trustedCertSources) {
-			copy.setTrustedCertSource(certificateSource);
+		if (certificateVerifier != null) {
+			copy.setDefaultDigestAlgorithm(certificateVerifier.getDefaultDigestAlgorithm());
+			copy.setDataLoader(certificateVerifier.getDataLoader());
+			copy.setCrlSource(certificateVerifier.getCrlSource());
+			copy.setOcspSource(certificateVerifier.getOcspSource());
+			copy.setCheckRevocationForUntrustedChains(certificateVerifier.isCheckRevocationForUntrustedChains());
+			copy.setAdjunctCertSource(certificateVerifier.getAdjunctCertSource());
+			List<CertificateSource> trustedCertSources = certificateVerifier.getTrustedCertSources();
+			for (CertificateSource certificateSource : trustedCertSources) {
+				copy.setTrustedCertSource(certificateSource);
+			}
+			copy.setAlertOnInvalidTimestamp(certificateVerifier.getAlertOnInvalidTimestamp());
+			copy.setAlertOnMissingRevocationData(certificateVerifier.getAlertOnMissingRevocationData());
+			copy.setAlertOnNoRevocationAfterBestSignatureTime(certificateVerifier.getAlertOnNoRevocationAfterBestSignatureTime());
+			copy.setAlertOnRevokedCertificate(certificateVerifier.getAlertOnRevokedCertificate());
+			copy.setAlertOnUncoveredPOE(certificateVerifier.getAlertOnUncoveredPOE());
 		}
-		copy.setAlertOnInvalidTimestamp(certificateVerifier.getAlertOnInvalidTimestamp());
-		copy.setAlertOnMissingRevocationData(certificateVerifier.getAlertOnMissingRevocationData());
-		copy.setAlertOnNoRevocationAfterBestSignatureTime(certificateVerifier.getAlertOnNoRevocationAfterBestSignatureTime());
-		copy.setAlertOnRevokedCertificate(certificateVerifier.getAlertOnRevokedCertificate());
-		copy.setAlertOnUncoveredPOE(certificateVerifier.getAlertOnUncoveredPOE());
 		return copy;
 	}
 	
 	public CertificateVerifier buildOfflineAndSilentCopy() {
 		CertificateVerifier offlineCertificateVerifier = new CommonCertificateVerifier(true);
-		offlineCertificateVerifier.setDefaultDigestAlgorithm(certificateVerifier.getDefaultDigestAlgorithm());
-		offlineCertificateVerifier.setAdjunctCertSource(certificateVerifier.getAdjunctCertSource());
-		List<CertificateSource> trustedCertSources = certificateVerifier.getTrustedCertSources();
-		for (CertificateSource certificateSource : trustedCertSources) {
-			offlineCertificateVerifier.setTrustedCertSource(certificateSource);
+		if (certificateVerifier != null) {
+			offlineCertificateVerifier.setDefaultDigestAlgorithm(certificateVerifier.getDefaultDigestAlgorithm());
+			offlineCertificateVerifier.setAdjunctCertSource(certificateVerifier.getAdjunctCertSource());
+			List<CertificateSource> trustedCertSources = certificateVerifier.getTrustedCertSources();
+			for (CertificateSource certificateSource : trustedCertSources) {
+				offlineCertificateVerifier.setTrustedCertSource(certificateSource);
+			}
 		}
 		// disable alerting
 		offlineCertificateVerifier.setAlertOnInvalidTimestamp(new SilentOnStatusAlert());
