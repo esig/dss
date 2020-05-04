6f1bd0f5dddc89364c08ef5504a08be23367da5c
SebastianSchuberth <sebastian.schuberth@ecsec.de>
2020-05-04 14:31:28 +0200

    Add tests for embedding and retrieval of signed assertions for CAdES and PAdES

---
+++ dss-pades/src/test/java/eu/europa/esig/dss/pades/signature/suite/PAdESSignedAssertionTest.java
@@ -0,0 +1,91 @@
+/**
+ * DSS - Digital Signature Services
+ * Copyright (C) 2015 European Commission, provided under the CEF programme
+ *
+ * This file is part of the "DSS - Digital Signature Services" project.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package eu.europa.esig.dss.pades.signature.suite;
+
+import eu.europa.esig.dss.diagnostic.DiagnosticData;
+import eu.europa.esig.dss.diagnostic.SignatureWrapper;
+import eu.europa.esig.dss.diagnostic.jaxb.XmlSignedAssertion;
+import eu.europa.esig.dss.enumerations.SignatureLevel;
+import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.InMemoryDocument;
+import eu.europa.esig.dss.pades.PAdESSignatureParameters;
+import eu.europa.esig.dss.pades.PAdESTimestampParameters;
+import eu.europa.esig.dss.pades.signature.PAdESService;
+import eu.europa.esig.dss.signature.DocumentSignatureService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
+public class PAdESSignedAssertionTest extends AbstractPAdESTestSignature {
+
+	private DocumentSignatureService<PAdESSignatureParameters, PAdESTimestampParameters> service;
+	private PAdESSignatureParameters signatureParameters;
+	private DSSDocument documentToSign;
+	private final String signedAssertionString = "<saml2:Assertion>test</saml2:Assertion>"; // format does not matter here
+
+	@BeforeEach
+	public void init() throws Exception {
+		documentToSign = new InMemoryDocument(getClass().getResourceAsStream("/sample.pdf"));
+		signatureParameters = new PAdESSignatureParameters();
+		signatureParameters.setSigningCertificate(getSigningCert());
+		signatureParameters.setCertificateChain(getCertificateChain());
+		signatureParameters.setSignatureLevel(SignatureLevel.PAdES_BASELINE_B);
+		signatureParameters.bLevel().setSignedAssertions(Arrays.asList(signedAssertionString));
+
+		service = new PAdESService(getCompleteCertificateVerifier());
+	}
+
+	@Override
+	protected void verifyDiagnosticData(DiagnosticData diagnosticData) {
+		super.verifyDiagnosticData(diagnosticData);
+		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
+
+		// verify that SAML assertions were included successfully in signature
+		List<String> signedAssertions = new ArrayList<>();
+		for (XmlSignedAssertion xmlSignedAssertion : signature.getSignedAssertions()) {
+			signedAssertions.add(xmlSignedAssertion.getAssertion());
+		}
+		assertEquals(getSignatureParameters().bLevel().getSignedAssertions(), signedAssertions);
+	}
+
+	@Override
+	protected DocumentSignatureService<PAdESSignatureParameters, PAdESTimestampParameters> getService() {
+		return service;
+	}
+
+	@Override
+	protected PAdESSignatureParameters getSignatureParameters() {
+		return signatureParameters;
+	}
+
+	@Override
+	protected DSSDocument getDocumentToSign() {
+		return documentToSign;
+	}
+
+	@Override
+	protected String getSigningAlias() {
+		return GOOD_USER;
+	}
+
+}
