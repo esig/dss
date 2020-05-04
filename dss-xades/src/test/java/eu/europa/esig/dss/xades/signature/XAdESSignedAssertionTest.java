05d1fe131327b65af5bade906873d2fa1af3004a
SebastianSchuberth <sebastian.schuberth@ecsec.de>
2020-05-04 15:33:05 +0200

    Add test for embedding and retrieval of signed assertions for XAdES

---
+++ dss-xades/src/test/java/eu/europa/esig/dss/xades/signature/XAdESSignedAssertionTest.java
@@ -0,0 +1,115 @@
+/**
+ * DSS - Digital Signature Services
+ * Copyright (C) 2015 European Commission, provided under the CEF programme
+ * 
+ * This file is part of the "DSS - Digital Signature Services" project.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package eu.europa.esig.dss.xades.signature;
+
+import eu.europa.esig.dss.DomUtils;
+import eu.europa.esig.dss.diagnostic.DiagnosticData;
+import eu.europa.esig.dss.diagnostic.SignatureWrapper;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import eu.europa.esig.dss.enumerations.SignatureLevel;
+import eu.europa.esig.dss.enumerations.SignaturePackaging;
+import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.FileDocument;
+import eu.europa.esig.dss.signature.DocumentSignatureService;
+import eu.europa.esig.dss.spi.DSSUtils;
+import eu.europa.esig.dss.validation.timestamp.TimestampToken;
+import eu.europa.esig.dss.xades.XAdESSignatureParameters;
+import eu.europa.esig.dss.xades.XAdESTimestampParameters;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+
+public class XAdESSignedAssertionTest extends AbstractXAdESTestSignature {
+
+	private static final Logger LOG = LoggerFactory.getLogger(XAdESSignedAssertionTest.class);
+	private DocumentSignatureService<XAdESSignatureParameters, XAdESTimestampParameters> service;
+	private DSSDocument documentToSign;
+
+	private Date signingDate;
+	private TimestampToken contentTimestamp;
+	private String signedAssertion;
+
+	@BeforeEach
+	public void init() throws Exception {
+		service = new XAdESService(getCompleteCertificateVerifier());
+		service.setTspSource(getGoodTsa());
+
+		documentToSign = new FileDocument(new File("src/test/resources/sample.xml"));
+		signedAssertion = new String(DSSUtils.toByteArray(getClass().getResourceAsStream("/sample-saml-assertion.xml")));
+
+		signingDate = new Date();
+		contentTimestamp = service.getContentTimestamp(documentToSign, getSignatureParameters());
+	}
+
+	@Override
+	protected DocumentSignatureService<XAdESSignatureParameters, XAdESTimestampParameters> getService() {
+		return service;
+	}
+
+	@Override
+	protected XAdESSignatureParameters getSignatureParameters() {
+		// Stateless mode
+		XAdESSignatureParameters signatureParameters = new XAdESSignatureParameters();
+		signatureParameters.bLevel().setSigningDate(signingDate);
+		signatureParameters.setSigningCertificate(getSigningCert());
+		signatureParameters.setCertificateChain(getCertificateChain());
+		signatureParameters.setSignaturePackaging(SignaturePackaging.ENVELOPING);
+		 // sign with Level LTA to check that correct ds:Signature element (NOT in signed assertion) is extended
+		signatureParameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_LTA);
+		signatureParameters.bLevel().setSignedAssertions(Arrays.asList(signedAssertion));
+		if (contentTimestamp != null) {
+			signatureParameters.setContentTimestamps(Arrays.asList(contentTimestamp));
+		}
+		return signatureParameters;
+	}
+	
+	@Override
+	protected void verifyDiagnosticData(DiagnosticData diagnosticData) {
+		super.verifyDiagnosticData(diagnosticData);
+		SignatureWrapper signature = diagnosticData.getSignatureById(diagnosticData.getFirstSignatureId());
+
+		// verify that SAML assertions were included successfully in signature
+		List<String> signedAssertions = getSignatureParameters().bLevel().getSignedAssertions();
+		for (int i = 0; i < signedAssertions.size(); i++) {
+			Document expected = DomUtils.buildDOM(signedAssertions.get(i));
+			Document extracted = DomUtils.buildDOM(signature.getSignedAssertions().get(i).getAssertion());
+			assertTrue(expected.isEqualNode(extracted));
+		}
+	}
+
+	@Override
+	protected DSSDocument getDocumentToSign() {
+		return documentToSign;
+	}
+
+	@Override
+	protected String getSigningAlias() {
+		return GOOD_USER;
+	}
+
+}
