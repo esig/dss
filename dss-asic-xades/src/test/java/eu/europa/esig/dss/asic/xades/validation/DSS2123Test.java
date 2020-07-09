f405e0a3acaff6758c3fb250c4a4316d9ab84c20
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2020-07-09 12:29:58 +0200

    DSS-2123 : OCSPCertificateSource add a method to retrieve the signing certificate(s) based on the ResponderId + unit tests

---
+++ dss-asic-xades/src/test/java/eu/europa/esig/dss/asic/xades/validation/DSS2123Test.java
@@ -0,0 +1,72 @@
+package eu.europa.esig.dss.asic.xades.validation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import eu.europa.esig.dss.diagnostic.CertificateRevocationWrapper;
+import eu.europa.esig.dss.diagnostic.CertificateWrapper;
+import eu.europa.esig.dss.diagnostic.DiagnosticData;
+import eu.europa.esig.dss.diagnostic.TimestampWrapper;
+import eu.europa.esig.dss.enumerations.RevocationType;
+import eu.europa.esig.dss.enumerations.SignatureLevel;
+import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.InMemoryDocument;
+import eu.europa.esig.dss.utils.Utils;
+
+public class DSS2123Test extends AbstractASiCWithXAdESTestValidation {
+
+	@Override
+	protected DSSDocument getSignedDocument() {
+		return new InMemoryDocument(getClass().getResourceAsStream("/validation/DSS-2123.asice"));
+	}
+	@Override
+	protected void checkRevocationData(DiagnosticData diagnosticData) {
+		super.checkRevocationData(diagnosticData);
+		
+		boolean revocationWithOtherIssuerFound = false;
+		
+		for (CertificateWrapper certificateWrapper : diagnosticData.getUsedCertificates()) {
+			CertificateWrapper signingCertificate = certificateWrapper.getSigningCertificate();
+			if (signingCertificate == null) {
+				continue;
+			}
+			
+			List<CertificateRevocationWrapper> certificateRevocationData = certificateWrapper.getCertificateRevocationData();
+			if (Utils.isCollectionNotEmpty(certificateRevocationData)) {
+				for (CertificateRevocationWrapper certificateRevocationWrapper : certificateRevocationData) {
+					if (RevocationType.OCSP.equals(certificateRevocationWrapper.getRevocationType())) {
+						CertificateWrapper ocspSignCert = certificateRevocationWrapper.getSigningCertificate();
+						assertNotNull(ocspSignCert);
+						if (!signingCertificate.getId().equals(ocspSignCert.getId())) {
+							revocationWithOtherIssuerFound = true;
+							assertNotEquals(certificateWrapper.getEncryptionAlgorithm(), certificateRevocationWrapper.getEncryptionAlgorithm());
+						}
+					}
+				}
+			}
+		}
+		
+		assertTrue(revocationWithOtherIssuerFound);
+	}
+	
+	@Override
+	protected void checkSignatureLevel(DiagnosticData diagnosticData) {
+		assertEquals(SignatureLevel.XAdES_BASELINE_T, diagnosticData.getSignatureFormat(diagnosticData.getFirstSignatureId()));
+	}
+	
+	@Override
+	protected void checkTimestamps(DiagnosticData diagnosticData) {
+		List<TimestampWrapper> timestampList = diagnosticData.getTimestampList();
+		assertEquals(1, timestampList.size());
+		
+		TimestampWrapper timestampWrapper = timestampList.get(0);
+		assertTrue(timestampWrapper.isMessageImprintDataFound());
+		assertFalse(timestampWrapper.isMessageImprintDataIntact());
+	}
+
+}
