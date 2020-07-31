7e28e6fa9e8b04209a72df3137fe880cce042b5b
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2020-07-31 08:27:51 +0200

    DSS-2148 : add new packages to module-info

--- dss-model/src/main/java9/module-info.java
+++ dss-model/src/main/java9/module-info.java
@@ -2,4 +2,7 @@ module jpms_dss_model {
 	exports eu.europa.esig.dss.model;
 	exports eu.europa.esig.dss.model.identifier;
 	exports eu.europa.esig.dss.model.x509;
+	exports eu.europa.esig.dss.model.x509.revocation;
+	exports eu.europa.esig.dss.model.x509.revocation.crl;
+	exports eu.europa.esig.dss.model.x509.revocation.ocsp;
 }
