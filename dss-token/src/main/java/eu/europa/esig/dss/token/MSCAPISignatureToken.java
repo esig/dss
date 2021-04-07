ad51b62d261bcd4827ff3909dea0cf87bdd4d37f
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2021-04-07 09:29:18 +0200

    DSS-2424 : remove hack in MSCAPISignatureToken

--- dss-token/src/main/java/eu/europa/esig/dss/token/MSCAPISignatureToken.java
+++ dss-token/src/main/java/eu/europa/esig/dss/token/MSCAPISignatureToken.java
@@ -20,20 +20,12 @@
  */
 package eu.europa.esig.dss.token;
 
+import eu.europa.esig.dss.model.DSSException;
+
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.KeyStore.PasswordProtection;
-import java.security.KeyStoreSpi;
-import java.security.cert.X509Certificate;
-import java.util.Collection;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import eu.europa.esig.dss.model.DSSException;
 
 /**
  * Class holding all MS CAPI API access logic.
@@ -41,74 +33,12 @@ import eu.europa.esig.dss.model.DSSException;
  */
 public class MSCAPISignatureToken extends AbstractKeyStoreTokenConnection {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MSCAPISignatureToken.class);
-
-	/**
-	 * This method is a workaround for scenarios when multiple entries have the same alias. Since the alias is the only
-	 * "official" way of retrieving an entry, only the first entry with a given alias is accessible.
-	 * See:
-	 * https://joinup.ec.europa.eu/software/sd-dss/issue/problem-possible-keystore-aliases-collision-when-using-mscapi
-	 *
-	 * @param keyStore
-	 *            the key store to fix
-	 */
-	private static void _fixAliases(KeyStore keyStore) {
-		Field field;
-		KeyStoreSpi keyStoreVeritable;
-
-		try {
-			field = keyStore.getClass().getDeclaredField("keyStoreSpi");
-			field.setAccessible(true);
-			keyStoreVeritable = (KeyStoreSpi) field.get(keyStore);
-
-			if ("sun.security.mscapi.KeyStore$MY".equals(keyStoreVeritable.getClass().getName())) {
-
-				field = keyStoreVeritable.getClass().getEnclosingClass().getDeclaredField("entries");
-				field.setAccessible(true);
-				Object entriesObject = field.get(keyStoreVeritable);
-				if (entriesObject instanceof Map) {
-					// Old issue fixed in JDK 7u121 and JDK8
-					// More info :
-					// https://bugs.openjdk.java.net/browse/JDK-6483657
-					// http://hg.openjdk.java.net/jdk8u/jdk8u/jdk/rev/0901dc70ae2b
-					return;
-				} else if (entriesObject instanceof Collection<?>) {
-					Collection<?> entries = (Collection<?>) entriesObject;
-					String alias;
-					String hashCode;
-					X509Certificate[] certificates;
-
-					for (Object entry : entries) {
-						field = entry.getClass().getDeclaredField("certChain");
-						field.setAccessible(true);
-						certificates = (X509Certificate[]) field.get(entry);
-
-						hashCode = Integer.toString(certificates[0].hashCode());
-
-						field = entry.getClass().getDeclaredField("alias");
-						field.setAccessible(true);
-						alias = (String) field.get(entry);
-
-						if (!alias.equals(hashCode)) {
-							field.set(entry, alias.concat(" - ").concat(hashCode));
-						}
-					}
-				} else {
-					LOG.warn("Unsupported entries type : {}", entriesObject.getClass().getName());
-				}
-			}
-		} catch (Exception exception) {
-			LOG.error(exception.getMessage(), exception);
-		}
-	}
-
 	@Override
 	KeyStore getKeyStore() throws DSSException {
 		KeyStore keyStore = null;
 		try {
 			keyStore = KeyStore.getInstance("Windows-MY");
 			keyStore.load(null, null);
-			_fixAliases(keyStore);
 		} catch (IOException | GeneralSecurityException e) {
 			throw new DSSException("Unable to load MS CAPI keystore", e);
 		}
