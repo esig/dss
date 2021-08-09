a72e7390099d7a17486bf7da044666e708523098
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2021-08-09 10:24:19 +0200

    DSS-2539, DSS-2493 : add unit tests
    (cherry picked from commit b20f9222f9b259bdb7fcb5893e318527f4be7ba6)

---
+++ dss-xades/src/test/java/eu/europa/esig/dss/xades/signature/XAdESDoubleSignBAndExtendToLTATest.java
@@ -0,0 +1,104 @@
+package eu.europa.esig.dss.xades.signature;
+
+import eu.europa.esig.dss.diagnostic.DiagnosticData;
+import eu.europa.esig.dss.diagnostic.SignatureWrapper;
+import eu.europa.esig.dss.enumerations.SignatureLevel;
+import eu.europa.esig.dss.enumerations.SignaturePackaging;
+import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.FileDocument;
+import eu.europa.esig.dss.signature.DocumentSignatureService;
+import eu.europa.esig.dss.validation.AdvancedSignature;
+import eu.europa.esig.dss.xades.XAdESSignatureParameters;
+import eu.europa.esig.dss.xades.XAdESTimestampParameters;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class XAdESDoubleSignBAndExtendToLTATest extends AbstractXAdESTestSignature {
+
+    private DSSDocument originalDocument;
+
+    private DocumentSignatureService<XAdESSignatureParameters, XAdESTimestampParameters> service;
+    private XAdESSignatureParameters signatureParameters;
+    private DSSDocument documentToSign;
+
+    @BeforeEach
+    public void init() throws Exception {
+        service = new XAdESService(getCompleteCertificateVerifier());
+        service.setTspSource(getGoodTsa());
+
+        originalDocument = new FileDocument(new File("src/test/resources/sample.xml"));
+    }
+
+    @Override
+    protected DSSDocument sign() {
+        signatureParameters = new XAdESSignatureParameters();
+        signatureParameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_B);
+        documentToSign = originalDocument;
+        DSSDocument signedDocument = super.sign();
+
+        signatureParameters = new XAdESSignatureParameters();
+        signatureParameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_B);
+        documentToSign = signedDocument;
+        DSSDocument doubleSignedDocument = super.sign();
+
+        XAdESSignatureParameters extensionParameters = new XAdESSignatureParameters();
+        extensionParameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_LTA);
+        DSSDocument extendedDocument = service.extendDocument(doubleSignedDocument, extensionParameters);
+
+        signatureParameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_LTA);
+        documentToSign = originalDocument;
+        return extendedDocument;
+    }
+
+    @Override
+    protected void checkNumberOfSignatures(DiagnosticData diagnosticData) {
+        assertEquals(2, diagnosticData.getSignatures().size());
+    }
+
+    @Override
+    protected void checkAdvancedSignatures(List<AdvancedSignature> signatures) {
+        // skip
+    }
+
+    @Override
+    protected void checkSignatureLevel(DiagnosticData diagnosticData) {
+        super.checkSignatureLevel(diagnosticData);
+
+        for (SignatureWrapper signature : diagnosticData.getSignatures()) {
+            assertEquals(SignatureLevel.XAdES_BASELINE_LTA, signature.getSignatureFormat());
+        }
+    }
+
+    @Override
+    protected void checkSigningDate(DiagnosticData diagnosticData) {
+        // skip
+    }
+
+    @Override
+    protected DSSDocument getDocumentToSign() {
+        return documentToSign;
+    }
+
+    @Override
+    protected DocumentSignatureService<XAdESSignatureParameters, XAdESTimestampParameters> getService() {
+        return service;
+    }
+
+    @Override
+    protected XAdESSignatureParameters getSignatureParameters() {
+        signatureParameters.setSigningCertificate(getSigningCert());
+        signatureParameters.setCertificateChain(getCertificateChain());
+        signatureParameters.setSignaturePackaging(SignaturePackaging.ENVELOPED);
+        return signatureParameters;
+    }
+
+    @Override
+    protected String getSigningAlias() {
+        return GOOD_USER;
+    }
+
+}
