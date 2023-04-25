971501e17cdea3a9756639bc8ccb18503cdafe37
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2023-04-25 15:41:00 +0200

    remove unused import
    (cherry picked from commit 0b98ad8a26e2c4411bfa9a8270076a530686703d)

--- dss-document/src/main/java/eu/europa/esig/dss/validation/DiagnosticDataBuilder.java
+++ dss-document/src/main/java/eu/europa/esig/dss/validation/DiagnosticDataBuilder.java
@@ -136,7 +136,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Contains a common code for DiagnosticData building
