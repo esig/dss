3307967bfe34f5de87309c3a4253e57e55ecaf5c
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2023-08-22 09:13:36 +0200

    add unit test with a data object group

--- dss-evidence-record-common/src/test/java/eu/europa/esig/dss/evidencerecord/common/validation/AbstractEvidenceRecordTestValidation.java
+++ dss-evidence-record-common/src/test/java/eu/europa/esig/dss/evidencerecord/common/validation/AbstractEvidenceRecordTestValidation.java
@@ -2,8 +2,11 @@ package eu.europa.esig.dss.evidencerecord.common.validation;
 
 import eu.europa.esig.dss.diagnostic.DiagnosticData;
 import eu.europa.esig.dss.enumerations.DigestMatcherType;
+import eu.europa.esig.dss.enumerations.Indication;
+import eu.europa.esig.dss.enumerations.SubIndication;
 import eu.europa.esig.dss.model.DSSDocument;
 import eu.europa.esig.dss.model.ReferenceValidation;
+import eu.europa.esig.dss.simplereport.SimpleReport;
 import eu.europa.esig.dss.spi.x509.tsp.TimestampToken;
 import eu.europa.esig.dss.test.validation.AbstractDocumentTestValidation;
 import eu.europa.esig.dss.utils.Utils;
@@ -16,6 +19,10 @@ import eu.europa.esig.validationreport.jaxb.ValidationReportType;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public abstract class AbstractEvidenceRecordTestValidation extends AbstractDocumentTestValidation {
@@ -92,6 +99,72 @@ public abstract class AbstractEvidenceRecordTestValidation extends AbstractDocum
                 ++tstCounter;
             }
         }
+    }
+
+    protected void verifySimpleReport(SimpleReport simpleReport) {
+        assertNotNull(simpleReport);
+
+        List<String> signatureIdList = simpleReport.getSignatureIdList();
+        assertEquals(simpleReport.getSignaturesCount(), signatureIdList.size());
+
+        int numberOfValidSignatures = 0;
+        for (String sigId : signatureIdList) {
+            Indication indication = simpleReport.getIndication(sigId);
+            assertNotNull(indication);
+            assertTrue(Indication.TOTAL_PASSED.equals(indication) || Indication.INDETERMINATE.equals(indication)
+                    || Indication.TOTAL_FAILED.equals(indication));
+            if (Indication.TOTAL_PASSED.equals(indication)) {
+                assertTrue(Utils.isCollectionNotEmpty(simpleReport.getSignatureScopes(sigId)));
+
+                assertNull(simpleReport.getSubIndication(sigId));
+                assertTrue(Utils.isCollectionEmpty(simpleReport.getAdESValidationErrors(sigId)));
+
+                assertNotNull(simpleReport.getSignatureExtensionPeriodMax(sigId));
+                ++numberOfValidSignatures;
+
+            } else {
+                SubIndication subIndication = simpleReport.getSubIndication(sigId);
+                assertNotNull(subIndication);
+                assertFalse(Utils.isCollectionEmpty(simpleReport.getAdESValidationErrors(sigId)));
+
+                if (SubIndication.TRY_LATER.equals(subIndication)) {
+                    assertNotNull(simpleReport.getSignatureExtensionPeriodMax(sigId));
+                }
+            }
+            assertNotNull(simpleReport.getSignatureQualification(sigId));
+
+            List<eu.europa.esig.dss.simplereport.jaxb.XmlTimestamp> signatureTimestamps = simpleReport.getSignatureTimestamps(sigId);
+            for (eu.europa.esig.dss.simplereport.jaxb.XmlTimestamp xmlTimestamp : signatureTimestamps) {
+                String tstId = xmlTimestamp.getId();
+                assertNotNull(tstId);
+
+                Indication timestampIndication = simpleReport.getIndication(tstId);
+                assertNotNull(timestampIndication);
+                assertTrue(Indication.PASSED.equals(timestampIndication) || Indication.INDETERMINATE.equals(timestampIndication)
+                        || Indication.FAILED.equals(timestampIndication));
+                if (timestampIndication != Indication.PASSED) {
+                    assertNotNull(simpleReport.getSubIndication(tstId));
+                    assertTrue(Utils.isCollectionNotEmpty(simpleReport.getAdESValidationErrors(tstId)));
+                }
+                assertNotNull(simpleReport.getTimestampQualification(tstId));
+            }
+        }
+        assertEquals(simpleReport.getValidSignaturesCount(), numberOfValidSignatures);
+
+        List<String> timestampIdList = simpleReport.getTimestampIdList();
+        for (String tstId : timestampIdList) {
+            Indication indication = simpleReport.getIndication(tstId);
+            assertNotNull(indication);
+            assertTrue(Indication.PASSED.equals(indication) || Indication.INDETERMINATE.equals(indication)
+                    || Indication.FAILED.equals(indication));
+            if (indication != Indication.PASSED) {
+                assertNotNull(simpleReport.getSubIndication(tstId));
+                assertTrue(Utils.isCollectionNotEmpty(simpleReport.getAdESValidationErrors(tstId)));
+            }
+            assertNotNull(simpleReport.getTimestampQualification(tstId));
+        }
+
+        assertNotNull(simpleReport.getValidationTime());
     }
 
     @Override
