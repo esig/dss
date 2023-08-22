1c18ff3d36947e64116d2b5a8c5ba218c2ac36b9
FaroukKraiem <farouk.kraiem@nowina.lu>
2023-08-22 09:58:33 +0200

    add missing keyEntity Utils

---
+++ dss-spi/src/main/java/eu/europa/esig/dss/spi/util/KeyEntityTSPUtils.java
@@ -0,0 +1,13 @@
+package eu.europa.esig.dss.spi.util;
+
+import eu.europa.esig.dss.model.DSSException;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableEntryException;
+
+public final class KeyEntityTSPUtils {
+
+
+}
