eca4ae01d86abd4dc6446c024bcf5f49c667338e
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2023-09-22 15:09:58 +0200

    add missing files
    (cherry picked from commit 6d2d483725a8a18c3ee68292496ccf4e3196b2ae)

---
+++ specs-xades/src/main/java/eu/europa/esig/xades/definition/xadesen/XAdESENElement.java
@@ -0,0 +1,54 @@
+package eu.europa.esig.xades.definition.xadesen;
+
+import eu.europa.esig.dss.jaxb.common.definition.DSSElement;
+import eu.europa.esig.dss.jaxb.common.definition.DSSNamespace;
+import eu.europa.esig.xades.definition.XAdESNamespace;
+
+public enum XAdESENElement implements DSSElement {
+
+    /** Defines ASN1EvidenceRecord */
+    ASN1_EVIDENCE_RECORD("ASN1EvidenceRecord"),
+
+    /** Defines XML EvidenceRecord */
+    EVIDENCE_RECORD("EvidenceRecord"),
+
+    /** Defines an EvidenceRecord container */
+    SEALING_EVIDENCE_RECORDS("SealingEvidenceRecords");
+
+    /** Namespace */
+    private final DSSNamespace namespace;
+
+    /** The tag name */
+    private final String tagName;
+
+    /**
+     * Default constructor
+     *
+     * @param tagName {@link String}
+     */
+    XAdESENElement(String tagName) {
+        this.tagName = tagName;
+        this.namespace = XAdESNamespace.XADES_EN;
+    }
+
+    @Override
+    public DSSNamespace getNamespace() {
+        return namespace;
+    }
+
+    @Override
+    public String getTagName() {
+        return tagName;
+    }
+
+    @Override
+    public String getURI() {
+        return namespace.getUri();
+    }
+
+    @Override
+    public boolean isSameTagName(String value) {
+        return tagName.equals(value);
+    }
+
+}
