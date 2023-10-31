2781145b02010f8f33e5cfe3d11278e28455e8ee
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2023-10-31 14:15:40 +0100

    fix jenkins build

--- dss-pki-factory-jaxb/src/test/java/eu/europa/esig/dss/pki/jaxb/AbstractTestJaxbPKI.java
+++ dss-pki-factory-jaxb/src/test/java/eu/europa/esig/dss/pki/jaxb/AbstractTestJaxbPKI.java
@@ -2,16 +2,16 @@ package eu.europa.esig.dss.pki.jaxb;
 
 import eu.europa.esig.dss.pki.jaxb.model.JAXBCertEntityRepository;
 import eu.europa.esig.dss.spi.DSSSecurityProvider;
+import eu.europa.esig.dss.utils.Utils;
 
 import java.io.File;
-import java.net.URL;
 import java.security.Security;
-import java.util.Objects;
+import java.util.Collection;
 
 public abstract class AbstractTestJaxbPKI {
 
     protected static JAXBCertEntityRepository repository = new JAXBCertEntityRepository();
-    protected static final String XML_FOLDER = "pki";
+    protected static final String XML_FOLDER = "src/test/resources/pki";
 
     static {
         Security.addProvider(DSSSecurityProvider.getSecurityProvider());
@@ -19,18 +19,11 @@ public abstract class AbstractTestJaxbPKI {
     }
 
     private static void loadPki() {
-        for (File file : Objects.requireNonNull(getFolder().listFiles())) {
+        Collection<File> pkiFiles = Utils.listFiles(new File(XML_FOLDER), new String[]{"xml"}, false);
+        for (File file : pkiFiles) {
             JAXBPKILoader builder = new JAXBPKILoader();
             builder.persistPKI(repository, file);
         }
-    }
-
-    private static File getFolder() {
-        URL resourceFolder = AbstractTestJaxbPKI.class.getClassLoader().getResource(XML_FOLDER);
-        if (resourceFolder == null) {
-            throw new RuntimeException("PKI resource folder not found.");
-        }
-        return new File(resourceFolder.getFile());
     }
 
 }
--- dss-pki-factory-jaxb/src/test/java/eu/europa/esig/dss/pki/jaxb/PKIJaxbFacadeTest.java
+++ dss-pki-factory-jaxb/src/test/java/eu/europa/esig/dss/pki/jaxb/PKIJaxbFacadeTest.java
@@ -1,5 +1,6 @@
 package eu.europa.esig.dss.pki.jaxb;
 
+import eu.europa.esig.dss.utils.Utils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,6 +11,7 @@ import javax.xml.stream.XMLStreamException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -19,9 +21,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PKIJaxbFacadeTest extends AbstractTestJaxbPKI {
 
     public static Stream<Arguments> data() {
-        File folder = new File(PKIJaxbFacadeTest.class.getClassLoader().getResource(XML_FOLDER).getPath());
         List<Arguments> dataToRun = new ArrayList<>();
-        for (File file : folder.listFiles()) {
+        Collection<File> pkiFiles = Utils.listFiles(new File(XML_FOLDER), new String[]{"xml"}, false);
+        for (File file : pkiFiles) {
             dataToRun.add(Arguments.of(file));
         }
         return dataToRun.stream();
