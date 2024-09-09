dcb27b093b16060626065dc72d6054bca9b3da1a
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2024-09-09 11:24:51 +0200

    DSS-3249 : execute spotless
    (cherry picked from commit d434bb27b8aec641098231d46fa7d31297a9d81c)

--- dss-xades/src/test/java/eu/europa/esig/dss/xades/signature/XAdESLevelLTTstRevocationFreshnessNextUpdateFailTest.java
+++ dss-xades/src/test/java/eu/europa/esig/dss/xades/signature/XAdESLevelLTTstRevocationFreshnessNextUpdateFailTest.java
@@ -41,9 +41,7 @@ import org.junit.jupiter.api.BeforeEach;
 import java.io.File;
 import java.util.Calendar;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
