a810253b6c867e3cff05c53ccb7fb2c47b8bb79d
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2024-11-07 08:29:14 +0100

    spotless update

--- dss-pades-openpdf/src/test/java/eu/europa/esig/dss/pdf/openpdf/ITextDocumentReaderTest.java
+++ dss-pades-openpdf/src/test/java/eu/europa/esig/dss/pdf/openpdf/ITextDocumentReaderTest.java
@@ -1,19 +1,19 @@
 /**
  * DSS - Digital Signature Services
  * Copyright (C) 2015 European Commission, provided under the CEF programme
- * 
+ * <p>
  * This file is part of the "DSS - Digital Signature Services" project.
- * 
+ * <p>
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ * <p>
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ * <p>
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -36,7 +36,6 @@ import com.lowagie.text.pdf.PdfObject;
 import eu.europa.esig.dss.model.DSSDocument;
 import eu.europa.esig.dss.model.InMemoryDocument;
 import eu.europa.esig.dss.pades.PAdESSignatureParameters;
-import eu.europa.esig.dss.pdf.PdfMemoryUsageSetting;
 import eu.europa.esig.dss.test.PKIFactoryAccess;
 
 class ITextDocumentReaderTest extends PKIFactoryAccess {
